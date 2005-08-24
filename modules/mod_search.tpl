2ea2051de9ffcaa723a654b0dd064be1fb21a77d
Christian Fowler <spider@viovio.com>
2005-08-24 14:39:17 +0000

    add missing ticks

--- modules/mod_search.tpl
+++ modules/mod_search.tpl
@@ -1,12 +1,8 @@
 {bitmodule title=$moduleTitle|default:'Search' name="search"}
 {form ipackage=bitcommerce iname="quick_find" ifile="index.php?main_page=advanced_search_result"}
-	<div class="row">
 		<input type="hidden" name="search_in_description" value="1" />
-		<input type="text" name="keyword" />
-	</div>
-	<div class="row submit">
-		<input type="submit" name="search" {tr}Search{/tr} />
-	</div>
+		<input type="text" name="keyword" size="15" />
+		<input type="submit" name="search" value="{tr}Search{/tr}" />
 
 	<div class="row">
   <a href="{$smarty.const.HTTP_SERVER}{$smarty.const.BITCOMMERCE_PKG_URL}index.php?main_page=advanced_search">{tr}Advanced Search{/tr}</a>
--- pages/product_info/main_template_vars.php
+++ pages/product_info/main_template_vars.php
@@ -17,13 +17,13 @@
 // | to obtain it through the world-wide-web, please send a note to       |
 // | license@zen-cart.com so we can mail you a copy immediately.          |
 // +----------------------------------------------------------------------+
-// $Id: main_template_vars.php,v 1.9 2005/08/24 12:17:01 lsces Exp $
+// $Id: main_template_vars.php,v 1.10 2005/08/24 14:39:17 spiderr Exp $
 //
 
   $sql = "select count(*) as total
           from " . TABLE_PRODUCTS . " p, " .
                    TABLE_PRODUCTS_DESCRIPTION . " pd
-          where    p.`products_status = '1'
+          where    p.`products_status` = '1'
           and      p.`products_id` = '" . (int)$_GET['products_id'] . "'
           and      pd.`products_id` = p.`products_id`
           and      pd.`language_id` = '" . (int)$_SESSION['languages_id'] . "'";
@@ -57,7 +57,7 @@
                   p.`products_quantity_order_max`,
                   p.`products_discount_type`, p.`products_discount_type_from`, p.`products_sort_order`, p.`products_price_sorter`, m.manufacturers_name
            from   " . TABLE_PRODUCTS . " p LEFT OUTER JOIN " . TABLE_MANUFACTURERS ." m ON (p.`manufacturers_id`=m.`manufacturers_id`), " . TABLE_PRODUCTS_DESCRIPTION . " pd
-           where  p.`products_status = '1'
+           where  p.`products_status` = '1'
            and    p.`products_id` = '" . (int)$_GET['products_id'] . "'
            and    pd.`products_id` = p.`products_id`
            and    pd.`language_id` = '" . (int)$_SESSION['languages_id'] . "'";
