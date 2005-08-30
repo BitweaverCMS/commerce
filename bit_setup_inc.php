640940628a391485ba4d69bfe58e56a9bde44848
Christian Fowler <spider@viovio.com>
2005-08-30 16:35:59 +0000

    add commerce to some services

--- bit_setup_inc.php
+++ bit_setup_inc.php
@@ -15,10 +15,24 @@ if( !defined( 'BITCOMMERCE_DB_PREFIX' ) ) {
 	define( 'BITCOMMERCE_DB_PREFIX', $prefix.'com_' );
 }
 
-	require_once( BITCOMMERCE_PKG_PATH.'includes/classes/shopping_cart.php' );
 if( $gBitSystem->isPackageActive( 'bitcommerce' ) ) {
 	// include shopping cart class
-	require_once( BITCOMMERCE_PKG_PATH.'includes/classes/shopping_cart.php' );
+// 	require_once( BITCOMMERCE_PKG_PATH.'includes/classes/shopping_cart.php' );
+	$gLibertySystem->registerService( LIBERTY_SERVICE_COMMERCE, BITCOMMERCE_PKG_NAME, array(
+		'content_expunge_function' => 'bitcommerce_expunge',
+	) );
+}
+
+function bitcommerce_expunge ( &$pObject ) {
+	require_once( BITCOMMERCE_PKG_PATH.'includes/bitcommerce_start_inc.php' );
+	$relProduct = new CommerceProduct();
+
+	if( $relProduct->loadByRelatedContent( $pObject->mContentId ) ) {
+		if( !$relProduct->expunge() ) {
+			// we couldn't nuke the product because it was purchased
+		}
+	}
+
 }
 
 ?>
