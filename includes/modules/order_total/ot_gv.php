90768fa99d69f8ad988b0b683c32bf20d97b66b6
Christian Fowler <spider@viovio.com>
2005-09-13 13:38:25 +0000

    clean up gift certificate auto-amount

--- includes/modules/order_total/ot_gv.php
+++ includes/modules/order_total/ot_gv.php
@@ -17,7 +17,7 @@
 // | to obtain it through the world-wide-web, please send a note to       |
 // | license@zen-cart.com so we can mail you a copy immediately.          |
 // +----------------------------------------------------------------------+
-// $Id: ot_gv.php,v 1.6 2005/09/13 13:18:34 spiderr Exp $
+// $Id: ot_gv.php,v 1.7 2005/09/13 13:38:25 spiderr Exp $
 //
 
   class ot_gv {
@@ -41,9 +41,11 @@
       $this->credit_class = true;
 	  $gvAmount = $currencies->format( $this->user_has_gv_account( $_SESSION['customer_id'] ) );
 		if (!zen_not_null(ltrim($_SESSION['cot_gv'], ' 0')) || $_SESSION['cot_gv'] == '0') {
-			$_SESSION['cot_gv'] = preg_replace( '/[^\d\.]/', '', $gvAmount );
+			$gvNum = preg_replace( '/[^\d\.]/', '', $gvAmount );
+			if( is_numeric( $gvNum ) ) {
+				$_SESSION['cot_gv'] = ($order->info['total'] > $gvNum ? $gvNum : $order->info['total']);
+			}
 		}
-vd( $_SESSION['cot_gv'] );
       $this->checkbox = $this->user_prompt . '<input type="textfield" size="6" onChange="submitFunction()" name="cot_gv" value="' . number_format($_SESSION['cot_gv'], 2) . '">' . ($this->user_has_gv_account($_SESSION['customer_id']) > 0 ? '<br />' . MODULE_ORDER_TOTAL_GV_USER_BALANCE . $gvAmount : '');
       $this->output = array();
     }
