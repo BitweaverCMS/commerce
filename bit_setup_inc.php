79b7e840d33ab4180f3805771094ed257d32ed4d
Max Kremmel <xing@synapse.plus.com>
2005-09-16 17:45:24 +0000

    check for $gLibertyService availability - needed during install

--- bit_setup_inc.php
+++ bit_setup_inc.php
@@ -17,9 +17,11 @@ if( !defined( 'BITCOMMERCE_DB_PREFIX' ) ) {
 
 // include shopping cart class
 // 	require_once( BITCOMMERCE_PKG_PATH.'includes/classes/shopping_cart.php' );
-$gLibertySystem->registerService( LIBERTY_SERVICE_COMMERCE, BITCOMMERCE_PKG_NAME, array(
-	'content_expunge_function' => 'bitcommerce_expunge',
-) );
+if( !empty( $gLibertySystem ) ) {
+	$gLibertySystem->registerService( LIBERTY_SERVICE_COMMERCE, BITCOMMERCE_PKG_NAME, array(
+		'content_expunge_function' => 'bitcommerce_expunge',
+	) );
+}
 
 function bitcommerce_expunge ( &$pObject ) {
 	require_once( BITCOMMERCE_PKG_PATH.'includes/bitcommerce_start_inc.php' );
