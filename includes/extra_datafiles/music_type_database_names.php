87f0c5f49f09beff2e99acfbf0af3a080cf22b16
Christian Fowler <spider@viovio.com>
2006-04-19 18:02:32 +0000

    cleanup default table names

--- includes/extra_datafiles/music_type_database_names.php
+++ includes/extra_datafiles/music_type_database_names.php
@@ -17,18 +17,18 @@
 // | to obtain it through the world-wide-web, please send a note to       |
 // | license@zen-cart.com so we can mail you a copy immediately.          |
 // +----------------------------------------------------------------------+
-// $Id: music_type_database_names.php,v 1.4 2005/10/06 21:01:48 spiderr Exp $
+// $Id: music_type_database_names.php,v 1.5 2006/04/19 18:02:32 spiderr Exp $
 //
 
-define('TABLE_RECORD_ARTISTS', '`'.BITCOMMERCE_DB_PREFIX . 'record_artists`');
-define('TABLE_RECORD_ARTISTS_INFO', '`'.BITCOMMERCE_DB_PREFIX . 'record_artists_info`');
-define('TABLE_RECORD_COMPANY', '`'.BITCOMMERCE_DB_PREFIX . 'record_company`');
-define('TABLE_RECORD_COMPANY_INFO', '`'.BITCOMMERCE_DB_PREFIX . 'record_company_info`');
-define('TABLE_PRODUCT_MUSIC_EXTRA', '`'.BITCOMMERCE_DB_PREFIX . 'product_music_extra`');
-define('TABLE_MUSIC_GENRE', '`'.BITCOMMERCE_DB_PREFIX . 'music_genre`');
-define('TABLE_MUSIC_GENRE_INFO', '`'.BITCOMMERCE_DB_PREFIX . 'music_genre_info`');
-define('TABLE_MEDIA_MANAGER', '`'.BITCOMMERCE_DB_PREFIX . 'media_manager`');
-define('TABLE_MEDIA_TYPES', '`'.BITCOMMERCE_DB_PREFIX . 'media_types`');
-define('TABLE_MEDIA_CLIPS', '`'.BITCOMMERCE_DB_PREFIX . 'media_clips`');
-define('TABLE_MEDIA_TO_PRODUCTS', '`'.BITCOMMERCE_DB_PREFIX . 'media_to_products`');
+define('TABLE_RECORD_ARTISTS', '`'.BITCOMMERCE_DB_PREFIX . 'com_record_artists`');
+define('TABLE_RECORD_ARTISTS_INFO', '`'.BITCOMMERCE_DB_PREFIX . 'com_record_artists_info`');
+define('TABLE_RECORD_COMPANY', '`'.BITCOMMERCE_DB_PREFIX . 'com_record_company`');
+define('TABLE_RECORD_COMPANY_INFO', '`'.BITCOMMERCE_DB_PREFIX . 'com_record_company_info`');
+define('TABLE_PRODUCT_MUSIC_EXTRA', '`'.BITCOMMERCE_DB_PREFIX . 'com_product_music_extra`');
+define('TABLE_MUSIC_GENRE', '`'.BITCOMMERCE_DB_PREFIX . 'com_music_genre`');
+define('TABLE_MUSIC_GENRE_INFO', '`'.BITCOMMERCE_DB_PREFIX . 'com_music_genre_info`');
+define('TABLE_MEDIA_MANAGER', '`'.BITCOMMERCE_DB_PREFIX . 'com_media_manager`');
+define('TABLE_MEDIA_TYPES', '`'.BITCOMMERCE_DB_PREFIX . 'com_media_types`');
+define('TABLE_MEDIA_CLIPS', '`'.BITCOMMERCE_DB_PREFIX . 'com_media_clips`');
+define('TABLE_MEDIA_TO_PRODUCTS', '`'.BITCOMMERCE_DB_PREFIX . 'com_media_to_products`');
 ?>
