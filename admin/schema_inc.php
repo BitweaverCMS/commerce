e071671f05fbc7908c90f9298fdd84a8d2413a6f
Christian Fowler <spider@viovio.com>
2006-09-17 21:51:08 +0000

    update commerce perm names

--- admin/schema_inc.php
+++ admin/schema_inc.php
@@ -1225,9 +1225,9 @@ $gBitInstaller->registerPackageInfo( BITCOMMERCE_PKG_NAME, array(
 // ### Default UserPermissions
 $gBitInstaller->registerUserPermissions( BITCOMMERCE_PKG_NAME, array(
 	array('p_commerce_admin', 'Can administer commerce.', 'admin', 'bitcommerce'),
-	array('p_view_product', 'Can create and modify storefront products', 'basic', 'bitcommerce'),
-	array('p_purchase_product', 'Can purchase webstore products', 'basic', 'bitcommerce'),
-	array('p_edit_product', 'Can create and modify storefront products', 'editors', 'bitcommerce'),
+	array('p_commerce_view_product', 'Can create and modify storefront products', 'basic', 'bitcommerce'),
+	array('p_commerce_purchase_product', 'Can purchase webstore products', 'basic', 'bitcommerce'),
+	array('p_commerce_edit_product', 'Can create and modify storefront products', 'editors', 'bitcommerce'),
 ) );
 
 $gBitInstaller->registerSchemaDefault( BITCOMMERCE_PKG_NAME, array(
