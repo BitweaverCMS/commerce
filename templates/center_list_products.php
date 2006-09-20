337ba5689e7dd430f9fa86b1ee80b3ce6b02e920
Christian Fowler <spider@viovio.com>
2006-09-20 01:54:31 +0000

    fix dynamic.tpl

--- templates/center_list_products.php
+++ templates/center_list_products.php
@@ -13,26 +13,28 @@
 	if( !empty( $module_params['category_id'] ) ) {
 		$listHash['category_id'] = $module_params['category_id'];
 	}
+	if( !empty( $module_params['featured'] ) ) {
+		$listHash['featured'] = TRUE;
+	}
 	if( !empty( $module_params['commissioned'] ) ) {
 		$listHash['commissioned'] = TRUE;
 	}
 	$row = 0;
 	$col = 0;
 	$listBoxContents = '';
-
-	// show only when 1 or more
-	if( $commissionedProducts = $gBitProduct->getList( $listHash ) ) {
-		$num_products_count = count( $commissionedProducts );
+	if( $listedProducts = $gBitProduct->getList( $listHash ) ) {
+		// show only when 1 or more
+		$num_products_count = count( $listedProducts );
 		if( $num_products_count < $columnCount  ) {
 			$col_width = 100/$num_products_count;
 		} else {
 			$col_width = 100/$columnCount;
 		}
-		foreach( array_keys( $commissionedProducts ) as $productsId ) {
+		foreach( array_keys( $listedProducts ) as $productsId ) {
 			$products_price = CommerceProduct::getDisplayPrice( $productsId );
 			$listBoxContents[$row][$col] = array('align' => 'center',
 													'params' => 'class="smallText" width="' . $col_width . '%" valign="top"',
-													'text' => '<a href="' . CommerceProduct::getDisplayUrl( $productsId ) . '">' . zen_image( CommerceProduct::getImageUrl( $commissionedProducts[$productsId]['products_id'], 'avatar' ), $commissionedProducts[$productsId]['products_name'] ) . '</a><br /><a href="' . CommerceProduct::getDisplayUrl( $productsId ) . '">' . $commissionedProducts[$productsId]['products_name'] . '</a><br />' . $products_price);
+													'text' => '<a href="' . CommerceProduct::getDisplayUrl( $productsId ) . '">' . zen_image( CommerceProduct::getImageUrl( $listedProducts[$productsId]['products_id'], 'avatar' ), $listedProducts[$productsId]['products_name'] ) . '</a><br /><a href="' . CommerceProduct::getDisplayUrl( $productsId ) . '">' . $listedProducts[$productsId]['products_name'] . '</a><br />' . $products_price);
 
 			$col ++;
 			if ($col > ($columnCount - 1)) {
@@ -45,8 +47,8 @@
 			$category_title = zen_get_categories_name((int)$new_products_category_id);
 			$title =  $title . ($category_title != '' ? ' - ' . $category_title : '');
 		}
-		$gBitSmarty->assign( 'listBoxContents', $listBoxContents );
 		$gBitSmarty->assign( 'productListTitle', $title );
 	}
+	$gBitSmarty->assign( 'listBoxContents', $listBoxContents );
 
 ?>
