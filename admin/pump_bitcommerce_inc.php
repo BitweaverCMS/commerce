fdef8bc50f7988ea960cdc24b4284d57a7060d6e
Max Kremmel <xing@synapse.plus.com>
2007-07-09 13:16:11 +0000

    move pump files to individual packages

---
+++ admin/pump_bitcommerce_inc.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @version $Header: /cvsroot/bitweaver/_bit_commerce/admin/pump_bitcommerce_inc.php,v 1.1 2007/07/09 13:16:11 squareing Exp $
+ * @package install
+ * @subpackage pumps
+ */
+
+/**
+ * Required files
+ */
+	require_once( BITCOMMERCE_PKG_PATH.'includes/common_inc.php' );
+	reset_bitcommerce_layout();
+
+	$pumpedData['Bitcommerce'][] = 'Created Commerce Layout';
+
+?>
