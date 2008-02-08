04fdc9cbc8c4ba7341c74aa9a233534e5d614224
Christian Fowler <spider@viovio.com>
2008-02-08 04:43:06 +0000

    remove debug

--- pages/shopping_cart/shopping_cart.php
+++ pages/shopping_cart/shopping_cart.php
@@ -17,7 +17,7 @@
 // | to obtain it through the world-wide-web, please send a note to       |
 // | license@zen-cart.com so we can mail you a copy immediately.          |
 // +----------------------------------------------------------------------+
-// $Id: shopping_cart.php,v 1.15 2008/01/02 04:44:24 spiderr Exp $
+// $Id: shopping_cart.php,v 1.16 2008/02/08 04:43:06 spiderr Exp $
 //
 ?>
 <script language="javascript" src="includes/general.js" type="text/javascript"></script>
@@ -40,16 +40,6 @@ function popupWindow(url) {
     }
     if (SHOW_TOTALS_IN_CART == '2') {
       echo TEXT_TOTAL_ITEMS . $_SESSION['cart']->count_contents() . ($_SESSION['cart']->show_weight() > 0 ? TEXT_TOTAL_WEIGHT . $_SESSION['cart']->show_weight() . TEXT_SHIPPING_WEIGHT : '') . TEXT_TOTAL_AMOUNT . $currencies->format($_SESSION['cart']->show_total()) . '<br />';
-    }
-    // used for testing
-    if (false) {
-      echo 'I SHOW IN CART MIXED Product ID 168 on ' . $_SESSION['cart']->in_cart_mixed_discount_quantity('168:d89443fdf309475ce09268e1c1db12dc') . '<br>';
-      echo 'I SHOW IN CART MIXED Product ID 169 off ' . $_SESSION['cart']->in_cart_mixed_discount_quantity('169:d370574074572c79a9d0c96b069f6e32') . '<br>';
-
-      echo 'I AM GV ONLY ' . $_SESSION['cart']->gv_only() . ' - ' . $_SESSION['cart']->get_content_type() . '<br />';
-      echo 'Free Products: ' .  $_SESSION['cart']->in_cart_check('product_is_free','1') . '<br />';
-      echo 'Virtual Products: ' .  $_SESSION['cart']->in_cart_check('products_virtual','1') . '<br />';
-      echo 'Free Shipping Products: ' .  $_SESSION['cart']->in_cart_check('product_is_always_free_ship','1') . '<br />';
     }
   ?>
   </td>
