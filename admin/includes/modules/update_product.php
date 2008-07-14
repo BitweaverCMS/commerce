65c8f5c4390965b2df4afc33340b562e82c5850a
Lester Caine <lester@lsces.co.uk>
2008-07-14 13:17:40 +0000

    Roll back retaining fix for redirect link

--- admin/includes/modules/update_product.php
+++ admin/includes/modules/update_product.php
@@ -17,23 +17,19 @@
 // | to obtain it through the world-wide-web, please send a note to       |
 // | license@zen-cart.com so we can mail you a copy immediately.          |
 // +----------------------------------------------------------------------+
-//  $Id: update_product.php,v 1.10 2008/07/13 16:42:03 lsces Exp $
+//  $Id: update_product.php,v 1.11 2008/07/14 13:17:40 lsces Exp $
 //
 	if (isset($_POST['edit_x']) || isset($_POST['edit_y'])) {
 		$action = 'new_product';
 	} else {
 		$_REQUEST['category_id'] = $current_category_id;
 		$newProduct = new CommerceProduct();
-		// update with a full path so the image is copied to the proper place - if not already loaded
-		if( !empty( $_REQUEST['products_image'] ) && !is_numeric( $_REQUEST['products_image'] ) ) {
+		// update with a full path so the image is copied to the proper place
+		if( !empty( $_REQUEST['products_image'] ) ) {
 			$_REQUEST['products_image'] = STORAGE_PKG_PATH.BITCOMMERCE_PKG_NAME.'/images/'.$_REQUEST['products_image'];
-		}
-		if( !empty( $_REQUEST['products_image_att'] ) && is_numeric( $_REQUEST['products_image_att'] ) ) {
-			$_REQUEST['products_image'] = $_REQUEST['products_image_att'];
 		}
 
 		$newProduct->store( $_REQUEST );
-
 		zen_redirect(zen_href_link_admin(FILENAME_CATEGORIES, 'cPath=' . $cPath . '&pID=' . $newProduct->mProductsId . (isset($_GET['page']) ? '&page=' . $_GET['page'] : '')));
 	}
 ?>
