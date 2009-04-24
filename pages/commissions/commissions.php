acf3ee01e7bc8b0a4f0233c59cae42951a428eef
Christian Fowler <spider@viovio.com>
2009-04-24 19:09:27 +0000

    use methods migrated to CommerceComission

--- pages/commissions/commissions.php
+++ pages/commissions/commissions.php
@@ -2,6 +2,14 @@
 
 $gBitSystem->verifyPermission('p_commerce_retailer');
 
+require_once( BITCOMMERCE_PKG_PATH.'classes/CommerceCommission.php' );
+
+if( count( $_GET ) > 2 || count( $_POST ) > 2 ) {
+	$gBitUser->verifyTicket();
+}
+
+$commissionManager = new CommerceCommission();
+
 if( !empty( $_REQUEST['save_commission_settings'] ) ) {
 	$gBitUser->storePreference( 'commissions_payment_method', $_REQUEST['commissions_payment_method'] );
 	if( !empty( $_REQUEST['commissions_'.$_REQUEST['commissions_payment_method'].'_address'] ) ) {
@@ -9,7 +17,7 @@ if( !empty( $_REQUEST['save_commission_settings'] ) ) {
 	}
 }
 
-$gBitSmarty->assign_by_ref( 'commissionList', $gBitCustomer->getCommissionsHistory() );
+$gBitSmarty->assign_by_ref( 'commissionList', $commissionManager->getProductCommissionHistory( array( 'user_id' => $gBitCustomer->mCustomerId ) ) );
 if( $addresses = $gBitCustomer->getAddresses( $gBitCustomer->mCustomerId ) ) {
 	foreach( $addresses AS $addrId=>$addr ) {
 		$addressList[$addr['address_book_id']] = zen_address_format( zen_get_address_format_id( $addr['country_id'] ), $addr, 0, ' ', ' ' );
