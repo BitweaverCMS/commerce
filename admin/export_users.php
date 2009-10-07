aaee2ece20ef4646550b472bde669e61944146d4
Tyler Bello <tylerbello@users.sourceforge.net>
2009-10-07 20:18:07 +0000

    add unsubscribe list checking and verified email checking

--- admin/export_users.php
+++ admin/export_users.php
@@ -1,13 +1,17 @@
 <?php
    require_once '../../bit_setup_inc.php';
    
-	global $gBitDb;
+	global $gBitDb,$gBitSystem;
 	
-	$query = "SELECT email,real_name,user_id,cab.*,MAX(date_purchased) as last_purchase FROM users_users uu
+	$query = "SELECT uu.email,real_name,uu.user_id,cab.*,MAX(date_purchased) as last_purchase FROM users_users uu
+			  INNER JOIN users_groups_map ugm ON (ugm.user_id = uu.user_id) 
 			  LEFT JOIN com_address_book cab ON (uu.user_id = cab.customers_id)
 			  LEFT JOIN com_orders co ON (cab.customers_id = co.customers_id)
+			  LEFT JOIN mail_subscriptions ms ON (ms.user_id = uu.user_id)
+	 		  WHERE ugm.group_id = ? AND ms.unsubscribe_date IS NULL
 			  GROUP BY uu.email,uu.real_name,uu.user_id,cab.address_book_id,cab.customers_id,cab.entry_gender,cab.entry_company,cab.entry_firstname,cab.entry_lastname,cab.entry_street_address,cab.entry_suburb,cab.entry_postcode,cab.entry_city,cab.entry_state,cab.entry_country_id,cab.entry_zone_id,cab.entry_telephone";
-	$result = $gBitDb->getAll($query); 
+	$result = $gBitDb->getAll($query,array($gBitSystem->getConfig('users_validate_email_group'))); 
+
 	$users_export = array();
 	foreach ($result as $res){
 		$users_export[$res['email']] = $res; //done like this to overwrite cases of users with multiple addresses and thus multiple entries	
