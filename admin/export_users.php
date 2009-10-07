af5d7e712427e3489c66cfd5c747e033422e5940
Tyler Bello <tylerbello@users.sourceforge.net>
2009-10-07 19:46:10 +0000

    add export_users to cvs script

---
+++ admin/export_users.php
@@ -0,0 +1,23 @@
+<?php
+   require_once '../../bit_setup_inc.php';
+   
+	global $gBitDb;
+	
+	$query = "SELECT email,real_name,user_id,cab.*,MAX(date_purchased) as last_purchase FROM users_users uu
+			  LEFT JOIN com_address_book cab ON (uu.user_id = cab.customers_id)
+			  LEFT JOIN com_orders co ON (cab.customers_id = co.customers_id)
+			  GROUP BY uu.email,uu.real_name,uu.user_id,cab.address_book_id,cab.customers_id,cab.entry_gender,cab.entry_company,cab.entry_firstname,cab.entry_lastname,cab.entry_street_address,cab.entry_suburb,cab.entry_postcode,cab.entry_city,cab.entry_state,cab.entry_country_id,cab.entry_zone_id,cab.entry_telephone";
+	$result = $gBitDb->getAll($query); 
+	$users_export = array();
+	foreach ($result as $res){
+		$users_export[$res['email']] = $res; //done like this to overwrite cases of users with multiple addresses and thus multiple entries	
+	}
+		$csv_output = "EmailAddress,FirstName,LastName,Address,City,State,Zip,Extra 1,Extra 2\n";//Extra 1 = customer id //Extra 2 = last purchase date
+	foreach ($users_export as $user){
+		$csv_output .= "$user[email],$user[entry_firstname],$user[entry_lastname],$user[entry_street_address],$user[entry_city],$user[entry_state],$user[entry_postcode],$user[customers_id],$user[last_purchase]\n";
+	}
+   header("Content-type: application/vnd.ms-excel");
+   header("Content-disposition: filename=users_export" . date("Y-m-d") . ".xls");
+   print $csv_output;
+   exit;  
+?>
