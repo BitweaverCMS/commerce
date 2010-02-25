808a6888eee37c24f701471dd1a07100ab5ffb7e
Christian Fowler <spider@viovio.com>
2010-02-25 23:14:20 +0000

    cleanup export users to allows selection of export fields, use fputcsv for hygiene in a dirty world

--- admin/export_users.php
+++ admin/export_users.php
@@ -1,43 +1,146 @@
 <?php
-	require_once '../../kernel/setup_inc.php';
 	require('includes/application_top.php');
+	define('HEADING_TITLE', tra( 'Export Customers' ) );
 
    
 	global $gBitDb,$gBitSystem;
-	
-	$query = "SELECT uu.email,real_name,uu.user_id,cab.*,cc.countries_name,MAX(date_purchased) as last_purchase, ms.unsubscribe_date 
-			  FROM users_users uu
-			 	 INNER JOIN users_groups_map ugm ON (ugm.user_id = uu.user_id) 
-				 LEFT JOIN com_address_book cab ON (uu.user_id = cab.customers_id)
-				 LEFT JOIN " . TABLE_COUNTRIES . " cc ON (cc.countries_id = cab.entry_country_id)
-				 LEFT JOIN com_orders co ON (cab.customers_id = co.customers_id)
-				 LEFT JOIN mail_subscriptions ms ON (ms.user_id = uu.user_id)
-	 		  WHERE ugm.group_id = ? AND uu.user_id>0
-			  GROUP BY uu.email,uu.real_name,uu.user_id,cab.address_book_id,cab.customers_id,cab.entry_gender,cab.entry_company,cab.entry_firstname,cab.entry_lastname,cab.entry_street_address,cab.entry_suburb,cab.entry_postcode,cab.entry_city,cab.entry_state,cab.entry_country_id,cab.entry_zone_id,cab.entry_telephone,ms.unsubscribe_date,cc.countries_name
-				ORDER BY uu.user_id";
-	$result = $gBitDb->getAll($query,array($gBitSystem->getConfig('users_validate_email_group'))); 
 
-	$users_export = array();
-	foreach ($result as $res){
-		$users_export[$res['email']] = $res; //done like this to overwrite cases of users with multiple addresses and thus multiple entries	
+if( !empty( $_REQUEST['export'] ) ) {
+	$sql = "SELECT uu.email,real_name,uu.user_id,cab.*,ccou.countries_name,MIN(date_purchased) as first_purchase_date, MAX(date_purchased) as last_purchase_date, COUNT(co.orders_id) AS num_purchases, ". $gBitDb->SqlIntToTimestamp( 'ms.unsubscribe_date' ) ." AS `unsubscribe_date`
+			FROM users_users uu
+			 	 INNER JOIN `".BIT_DB_PREFIX."users_groups_map` ugm ON (ugm.user_id = uu.user_id) 
+				LEFT JOIN " . TABLE_CUSTOMERS . " cc ON (cc.`customers_id`=uu.`user_id)
+				 LEFT JOIN " . TABLE_ADDRESS_BOOK ." cab ON (cc.customers_default_address_id = cab.address_book_id)
+				 LEFT JOIN " . TABLE_COUNTRIES . " ccou ON (ccou.countries_id = cab.entry_country_id)
+				 LEFT JOIN " . TABLE_ORDERS . " co ON (cab.customers_id = co.customers_id)
+				 LEFT JOIN `".BIT_DB_PREFIX."mail_subscriptions` ms ON (ms.user_id = uu.user_id)
+	 		WHERE ugm.`group_id` = ? AND uu.`user_id`>0
+			GROUP BY uu.email,uu.real_name,uu.user_id,cab.address_book_id,cab.customers_id,cab.entry_gender,cab.entry_company,cab.entry_firstname,cab.entry_lastname,cab.entry_street_address,cab.entry_suburb,cab.entry_postcode,cab.entry_city,cab.entry_state,cab.entry_country_id,cab.entry_zone_id,cab.entry_telephone,ms.unsubscribe_date,ccou.countries_name
+			ORDER BY RANDOM(), uu.`user_id`";
+
+	$max = (!empty( $_REQUEST['num_records'] ) ? $_REQUEST['num_records'] : NULL );
+	if( $rs = $gBitDb->query($sql,array( $gBitSystem->getConfig('users_validate_email_group') ), $max ) )  {
+		$tempFH = tmpfile();
+		$headerSet = FALSE;
+		while( $row = $rs->fetchRow() ) {
+			$csvOutput = array();
+
+			// Email
+			if( !$headerSet ) { $header[] = 'email'; }
+			$csvOutput[] = $row['email'];
+
+			// Unsubscribe Date
+			if( !$headerSet ) { $header[] = 'unsubscribe_date'; }
+			$csvOutput[] = $row['unsubscribe_date'];
+
+			if( !empty( $_REQUEST['customers_id'] ) ) {
+				if( !$headerSet ) { $header[] = 'customers_id'; }
+				$csvOutput[] = $row['user_id'];
+			}
+			if( !empty( $_REQUEST['firstname'] ) ) {
+				if( !$headerSet ) { $header[] = 'first_name'; }
+				$firstName = '';
+				if( !empty( $row['entry_firstname'] ) ) {
+					$firstName = $row['entry_firstname'];
+				} elseif( strpos( $row['real_name'], ' ' ) ) {
+					$firstName = substr( $row['real_name'], 0, strpos( $row['real_name'], ' ' ) );
+				}
+				$csvOutput[] = ucwords( strtolower( trim( $firstName ) ) );
+			}
+			if( !empty( $_REQUEST['lastname'] ) ) {
+				if( !$headerSet ) { $header[] = 'last_name'; }
+				$lastName = '';
+				if( !empty( $row['entry_lastname'] ) ) {
+					$lastName = $row['entry_lastname'];
+				} elseif( strpos( $row['real_name'], ' ' ) ) {
+					$lastName = substr( $row['real_name'], strpos( $row['real_name'], ' ' ) + 1 );
+				}
+				$csvOutput[] = ucwords( strtolower( trim( $lastName ) ) );
+			}
+			if( !empty( $_REQUEST['company'] ) ) {
+				if( !$headerSet ) { $header[] = 'company'; }
+				$csvOutput[] = ucwords( strtolower( trim( $row['entry_company'] ) ) );
+			}
+			if( !empty( $_REQUEST['street_address'] ) ) {
+				if( !$headerSet ) { $header[] = 'address'; }
+				$address = $row['entry_street_address'];
+				if( !empty( $row['suburb'] ) ) {
+					$address .= '\,'.$row['entry_suburb'];
+				}
+				$csvOutput[] = $address;
+			}
+			if( !empty( $_REQUEST['city'] ) ) {
+				if( !$headerSet ) { $header[] = 'city'; }
+				$csvOutput[] = ucwords( strtolower( trim( $row['entry_city'] ) ) );
+			}
+			if( !empty( $_REQUEST['state'] ) ) {
+				if( !$headerSet ) { $header[] = 'state'; }
+				$csvOutput[] = ucwords( strtolower( trim( $row['entry_state'] ) ) );
+			}
+			if( !empty( $_REQUEST['zip'] ) ) {
+				if( !$headerSet ) { $header[] = 'zip'; }
+				$csvOutput[] = trim( strtoupper( $row['entry_postcode'] ) );
+			}
+			if( !empty( $_REQUEST['country'] ) ) {
+				if( !$headerSet ) { $header[] = 'country'; }
+				$csvOutput[] = ucwords( strtolower( trim( $row['countries_name'] ) ) );
+			}
+			if( !empty( $_REQUEST['registration_date'] ) ) {
+				if( !$headerSet ) { $header[] = 'registration_date'; }
+				$csvOutput[] = $row['registration_date'];
+			}
+			if( !empty( $_REQUEST['first_purchase_date'] ) ) {
+				if( !$headerSet ) { $header[] = 'first_purchase_date'; }
+				$csvOutput[] = $row['first_purchase_date'];
+			}
+			if( !empty( $_REQUEST['last_purchase_date'] ) ) {
+				if( !$headerSet ) { $header[] = 'last_purchase_date'; }
+				$csvOutput[] = $row['last_purchase_date'];
+			}
+			if( !empty( $_REQUEST['num_purchases'] ) ) {
+				if( !$headerSet ) { $header[] = 'num_purchases'; }
+				$csvOutput[] = $row['num_purchases'];
+			}
+			if( !empty( $_REQUEST['interests'] ) ) {
+				if( !$headerSet ) { $header[] = 'interests'; }
+				if( $interests = $gBitDb->getCol( "SELECT `interests_name` FROM " . TABLE_CUSTOMERS_INTERESTS_MAP . " cim INNER JOIN " . TABLE_CUSTOMERS_INTERESTS . " ci ON(ci.`interests_id`=cim.`interests_id`) WHERE cim.`customers_id`=?", array( $row['customers_id'] ) ) ) {
+					$csvOutput[] = implode( '\,', $interests );
+				} else {
+					$csvOutput[] = "";
+				}
+			}
+			if( !$headerSet ) {
+				fputcsv( $tempFH, $header );
+				$headerSet = TRUE;
+			}
+			fputcsv( $tempFH, $csvOutput );
+		}
 	}
+
 	//Benchmark email format
-/*		$csv_output = "EmailAddress,FirstName,LastName,Address,City,State,Zip,Extra 1,Extra 2\n";//Extra 1 = customer id //Extra 2 = last purchase date
+/*		$csvOutput = "EmailAddress,FirstName,LastName,Address,City,State,Zip,Extra 1,Extra 2\n";//Extra 1 = customer id //Extra 2 = last purchase date
 	foreach ($users_export as $user){
-		$csv_output .= "$user[email],$user[entry_firstname],$user[entry_lastname],$user[entry_street_address],$user[entry_city],$user[entry_state],$user[entry_postcode],$user[customers_id],$user[last_purchase]\n";
+		$csvOutput[] = "$user[email],$user[entry_firstname],$user[entry_lastname],$user[entry_street_address],$user[entry_city],$user[entry_state],$user[entry_postcode],$user[customers_id],$user[last_purchase]\n";
 	}*/
-	//Mad Mimi etc. format
-//	$csv_output = "Email,firstname,lastname,address,city,state,zip,country,organizationname,phone,user_id,last_purchase,opt out\n";
+	//Mad Mimi format
+// address	city	company	confirmed	country	unsubscribe_date	email	first_name	last_name	phone	state	title	zip	suppression_reason
+//	$csvOutput = "Email,firstname,lastname,address,city,state,zip,country,organizationname,phone,user_id,last_purchase,opt out\n";
 //	foreach ($users_export as $user){
-//		$csv_output .= "$user[email],$user[entry_firstname],$user[entry_lastname],$user[entry_street_address],$user[entry_city],$user[entry_state],$user[entry_postcode],$user[user_id],$user[last_purchase],".(!empty($user['unsubscribe_date'])?'1':'')."\n";
+//		$csvOutput[] = "$user[email],$user[entry_firstname],$user[entry_lastname],$user[entry_street_address],$user[entry_city],$user[entry_state],$user[entry_postcode],$user[user_id],$user[last_purchase],".(!empty($user['unsubscribe_date'])?'1':'')."\n";
 //	}
-	$csv_output = "Email,opt out\n";
-	foreach ($users_export as $user){
-		$csv_output .= "$user[email],".(!empty($user['unsubscribe_date'])?'1':'')."\n";
-	}
 
-   header("Content-type: text/csv");
-   header("Content-disposition: filename=users_export" . date("Y-m-d") . ".csv");
-   print $csv_output;
-   exit;  
+	header("Content-type: text/csv");
+	header("Content-disposition: filename=users_export" . date("Y-m-d") . ".csv");
+	fseek( $tempFH, 0 );	
+	fpassthru( $tempFH );
+
+	print $csvOutput;
+	exit;  
+}
+
+print $gBitSmarty->fetch( 'bitpackage:bitcommerce/admin_export_users.tpl' );
+
+require(DIR_FS_ADMIN_INCLUDES . 'footer.php'); 
+require(DIR_FS_ADMIN_INCLUDES . 'application_bottom.php'); 
+
 ?>
---
+++ templates/admin_export_users.tpl
@@ -0,0 +1,126 @@
+<h1 class="header">
+	{tr}Export Customers{/tr}
+</h1>
+
+<div class="body customerexport">
+{literal}
+<style type="text/css">
+.row .formlabel {text-align:right; width:14em;float:left; }
+.row .forminput {margin-left: 15em;}
+</style>
+{/literal}
+
+{form}
+<div class="row">
+	{formlabel label="First Name"}
+	{forminput}
+		<input type="checkbox" name="firstname" value="y" checked="checked" />
+	{/forminput}
+</div>
+<div class="row">
+	{formlabel label="Last Name"}
+	{forminput}
+		<input type="checkbox" name="lastname" value="y" checked="checked" />
+	{/forminput}
+</div>
+<div class="row">
+	{formlabel label="Company"}
+	{forminput}
+		<input type="checkbox" name="company" value="y" checked="checked" />
+	{/forminput}
+</div>
+<div class="row">
+	{formlabel label="Street Address"}
+	{forminput}
+		<input type="checkbox" name="street_address" value="y" checked="checked" />
+	{/forminput}
+</div>
+<div class="row">
+	{formlabel label="City"}
+	{forminput}
+		<input type="checkbox" name="city" value="y" checked="checked" />
+	{/forminput}
+</div>
+<div class="row">
+	{formlabel label="State"}
+	{forminput}
+		<input type="checkbox" name="state" value="y" checked="checked" />
+	{/forminput}
+</div>
+<div class="row">
+	{formlabel label="Postal Code"}
+	{forminput}
+		<input type="checkbox" name="zip" value="y" checked="checked" />
+	{/forminput}
+</div>
+<div class="row">
+	{formlabel label="Country"}
+	{forminput}
+		<input type="checkbox" name="country" value="y" checked="checked" />
+		{formhelp note=""}
+	{/forminput}
+</div>
+<div class="row">
+	{formlabel label="Customer Id"}
+	{forminput}
+		<input type="checkbox" name="customers_id" value="y" checked="checked" />
+		{formhelp note=""}
+	{/forminput}
+</div>
+<div class="row">
+	{formlabel label="Registration Date"}
+	{forminput}
+		<input type="checkbox" name="registration_date" value="y" checked="checked" />
+		{formhelp note=""}
+	{/forminput}
+</div>
+<div class="row">
+	{formlabel label="First Purchase Date"}
+	{forminput}
+		<input type="checkbox" name="first_purchase_date" value="y" checked="checked" />
+		{formhelp note=""}
+	{/forminput}
+</div>
+<div class="row">
+	{formlabel label="Last Purchase Date"}
+	{forminput}
+		<input type="checkbox" name="last_purchase_date" value="y" checked="checked" />
+		{formhelp note=""}
+	{/forminput}
+</div>
+<div class="row">
+	{formlabel label="Number of Purchases"}
+	{forminput}
+		<input type="checkbox" name="num_purchases" value="y" checked="checked" />
+		{formhelp note=""}
+	{/forminput}
+</div>
+<div class="row">
+	{formlabel label="Registration Interests"}
+	{forminput}
+		<input type="checkbox" name="interests" value="y" {if $gBitSystem->isFeatureActive('commerce_register_interests')}checked="checked"{/if} />
+	{/forminput}
+</div>
+<div class="row">
+	{formlabel label="Number of Records"}
+	{forminput}
+		<input type="text" name="num_records" />
+		{formhelp note="Leave empty to export all records"}
+	{/forminput}
+</div>
+<div class="row">
+	{formlabel label="Format"}
+	{forminput}
+		CSV
+		{formhelp note=""}
+	{/forminput}
+</div>
+<div class="row submit">
+	{forminput}
+		<input type="submit" value="{tr}Export{/tr}" name="export"/>
+	{/forminput}
+</div>
+{/form}
+
+</div>
+
