2c0971d1d9f35215a6964eeca18ea0a23fc41660
Christian Fowler <spider@viovio.com>
2010-03-08 23:35:56 +0000

    copy logo's for banners in pump

--- admin/pump_bitcommerce_inc.php
+++ admin/pump_bitcommerce_inc.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * @version $Header: /cvsroot/bitweaver/_bit_commerce/admin/pump_bitcommerce_inc.php,v 1.1 2007/07/09 13:16:11 squareing Exp $
+ * @version $Header: /cvsroot/bitweaver/_bit_commerce/admin/pump_bitcommerce_inc.php,v 1.2 2010/03/08 23:35:56 spiderr Exp $
  * @package install
  * @subpackage pumps
  */
@@ -10,6 +10,10 @@
  */
 	require_once( BITCOMMERCE_PKG_PATH.'includes/common_inc.php' );
 	reset_bitcommerce_layout();
+
+	mkdir_p( STORAGE_PKG_PATH.'bitcommerce/images/banners/' );
+	copy( BITCOMMERCE_PKG_PATH.'images/banners/125bitcommerce_logo.gif', STORAGE_PKG_PATH.'bitcommerce/images/banners/125bitcommerce_logo.gif' );
+	copy( BITCOMMERCE_PKG_PATH.'images/banners/125x125_bitcommerce_logo.gif', STORAGE_PKG_PATH.'bitcommerce/images/banners/125x125_bitcommerce_logo.gif' );
 
 	$pumpedData['Bitcommerce'][] = 'Created Commerce Layout';
 
