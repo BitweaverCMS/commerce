be6cc414d6b28e0ed4cbe48f8e35cf35ae17e8bb
Christian Fowler <spider@viovio.com>
2010-05-13 23:36:13 +0000

    move session timeout of payment to top file before any processing

--- pages/checkout_process/checkout_process.php
+++ pages/checkout_process/checkout_process.php
@@ -1,66 +1,68 @@
 <?php
 //
 // +----------------------------------------------------------------------+
-// |zen-cart Open Source E-commerce                                       |
+// |zen-cart Open Source E-commerce																			 |
 // +----------------------------------------------------------------------+
-// | Copyright (c) 2003 The zen-cart developers                           |
-// |                                                                      |
-// | http://www.zen-cart.com/index.php                                    |
-// |                                                                      |
-// | Portions Copyright (c) 2003 osCommerce                               |
+// | Copyright (c) 2003 The zen-cart developers													 |
+// |																																			|
+// | http://www.zen-cart.com/index.php																		|
+// |																																			|
+// | Portions Copyright (c) 2003 osCommerce															 |
 // +----------------------------------------------------------------------+
-// | This source file is subject to version 2.0 of the GPL license,       |
-// | that is bundled with this package in the file LICENSE, and is        |
-// | available through the world-wide-web at the following url:           |
-// | http://www.zen-cart.com/license/2_0.txt.                             |
+// | This source file is subject to version 2.0 of the GPL license,			 |
+// | that is bundled with this package in the file LICENSE, and is				|
+// | available through the world-wide-web at the following url:					 |
+// | http://www.zen-cart.com/license/2_0.txt.														 |
 // | If you did not receive a copy of the zen-cart license and are unable |
-// | to obtain it through the world-wide-web, please send a note to       |
-// | license@zen-cart.com so we can mail you a copy immediately.          |
+// | to obtain it through the world-wide-web, please send a note to			 |
+// | license@zen-cart.com so we can mail you a copy immediately.					|
 // +----------------------------------------------------------------------+
-// $Id: checkout_process.php,v 1.7 2009/08/22 08:23:17 spiderr Exp $
+// $Id: checkout_process.php,v 1.8 2010/05/13 23:36:13 spiderr Exp $
 //
 
-  require_once(DIR_FS_MODULES . 'require_languages.php');
+	require_once(DIR_FS_MODULES . 'require_languages.php');
 
 // if the customer is not logged on, redirect them to the time out page
-  if (!$_SESSION['customer_id']) {
-    zen_redirect(zen_href_link(FILENAME_TIME_OUT));
-  }
+	if (!$_SESSION['customer_id']) {
+		zen_redirect(zen_href_link(FILENAME_TIME_OUT));
+	}
 
 // confirm where link came from
-  if (!strstr($_SERVER['HTTP_REFERER'], FILENAME_CHECKOUT_CONFIRMATION)) {
-//    zen_redirect(zen_href_link(FILENAME_CHECKOUT_PAYMENT,'','SSL'));
-  }
+	if (!strstr($_SERVER['HTTP_REFERER'], FILENAME_CHECKOUT_CONFIRMATION)) {
+//		zen_redirect(zen_href_link(FILENAME_CHECKOUT_PAYMENT,'','SSL'));
+	}
+
+	// keep session timeout of payment to top file before any processing
+	if (!isset($_SESSION['payment']) && !$credit_covers) {
+		zen_redirect(zen_href_link(FILENAME_DEFAULT));
+	}
 
 // load selected payment module
-  require(DIR_FS_CLASSES . 'payment.php');
-  $payment_modules = new payment($_SESSION['payment']);
+	require(DIR_FS_CLASSES . 'payment.php');
+	$payment_modules = new payment($_SESSION['payment']);
 // load the selected shipping module
-    require( BITCOMMERCE_PKG_PATH.'classes/CommerceShipping.php');
-  $shipping_modules = new CommerceShipping($_SESSION['shipping']);
+		require( BITCOMMERCE_PKG_PATH.'classes/CommerceShipping.php');
+	$shipping_modules = new CommerceShipping($_SESSION['shipping']);
 
-  require(DIR_FS_CLASSES . 'order.php');
-  $order = new order;
+	require(DIR_FS_CLASSES . 'order.php');
+	$order = new order;
 
-  require(DIR_FS_CLASSES . 'order_total.php');
-  $order_total_modules = new order_total;
-  $order_totals = $order_total_modules->pre_confirmation_check();
-  $order_totals = $order_total_modules->process();
+	require(DIR_FS_CLASSES . 'order_total.php');
+	$order_total_modules = new order_total;
+	$order_totals = $order_total_modules->pre_confirmation_check();
+	$order_totals = $order_total_modules->process();
 
-  if (!isset($_SESSION['payment']) && !$credit_covers) {
-    zen_redirect(zen_href_link(FILENAME_DEFAULT));
-  }
 $gBitDb->mDb->StartTrans();
 // load the before_process function from the payment modules
-  $payment_modules->before_process();
+	$payment_modules->before_process();
 
-  $insert_id = $order->create($order_totals, 2);
-  $order->create_add_products($insert_id);
+	$insert_id = $order->create($order_totals, 2);
+	$order->create_add_products($insert_id);
 
-  $payment_modules->after_order_create($insert_id);
+	$payment_modules->after_order_create($insert_id);
 
 $gBitDb->mDb->completeTrans();
 
-  $order->send_order_email($insert_id, 2);
+	$order->send_order_email($insert_id, 2);
 
 ?>
