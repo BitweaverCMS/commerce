c6657af079e5c37f11c4fc47f97b2e10b046ecf1
spiderr <spider@viovio.com>
2010-07-13 01:27:52 -0400

    pass in products_id to CommerceProduct::getImageUrl

--- includes/modules/product_listing.php
+++ includes/modules/product_listing.php
@@ -150,7 +150,7 @@
             if (isset($_GET['manufacturers_id'])) {
               $lc_text = '<a href="' . zen_href_link(zen_get_info_page($listing->fields['products_id']), 'manufacturers_id=' . $_GET['manufacturers_id'] . '&products_id=' . $listing->fields['products_id']) . '">' . zen_image(  CommerceProduct::getImageUrl( $listing->fields['products_id'], 'avatar' ), $listing->fields['products_name'] ) . '</a>';
             } else {
-              $lc_text = '&nbsp;<a href="' . CommerceProduct::getDisplayUrl( $listing->fields['products_id'] ) . '">' . zen_image( CommerceProduct::getImageUrl( $listing->fields, 'avatar' ), $listing->fields['products_name'] ) . '</a>&nbsp;';
+              $lc_text = '&nbsp;<a href="' . CommerceProduct::getDisplayUrl( $listing->fields['products_id'] ) . '">' . zen_image( CommerceProduct::getImageUrl( $listing->fields['products_id'], 'avatar' ), $listing->fields['products_name'] ) . '</a>&nbsp;';
             }
             break;
         }
