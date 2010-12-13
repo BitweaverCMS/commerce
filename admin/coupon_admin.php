b73b36c8e8c919b750c75718bd0a10e6056549d9
spiderr <spider@viovio.com>
2010-12-12 20:46:49 -0500

    add stats referer_url support

--- admin/coupon_admin.php
+++ admin/coupon_admin.php
@@ -1,21 +1,21 @@
 <?php
 //
 // +----------------------------------------------------------------------+
-// |zen-cart Open Source E-commerce																			 |
+// |zen-cart Open Source E-commerce											
 // +----------------------------------------------------------------------+
-// | Copyright (c) 2003 The zen-cart developers													 |
-// |																																			|
-// | http://www.zen-cart.com/index.php																		|
-// |																																			|
-// | Portions Copyright (c) 2003 osCommerce															 |
+// | Copyright (c) 2003 The zen-cart developers								
+// |																		
+// | http://www.zen-cart.com/index.php										
+// |																		
+// | Portions Copyright (c) 2003 osCommerce									
 // +----------------------------------------------------------------------+
-// | This source file is subject to version 2.0 of the GPL license,			 |
-// | that is bundled with this package in the file LICENSE, and is				|
-// | available through the world-wide-web at the following url:					 |
-// | http://www.zen-cart.com/license/2_0.txt.														 |
-// | If you did not receive a copy of the zen-cart license and are unable |
-// | to obtain it through the world-wide-web, please send a note to			 |
-// | license@zen-cart.com so we can mail you a copy immediately.					|
+// | This source file is subject to version 2.0 of the GPL license,			
+// | that is bundled with this package in the file LICENSE, and is			
+// | available through the world-wide-web at the following url:				
+// | http://www.zen-cart.com/license/2_0.txt.								
+// | If you did not receive a copy of the zen-cart license and are unable 
+// | to obtain it through the world-wide-web, please send a note to			
+// | license@zen-cart.com so we can mail you a copy immediately.			
 // +----------------------------------------------------------------------+
 //	$Id$
 //
@@ -72,17 +72,30 @@
 
 	switch ($getAction) {
 	case 'report':
-		BitBase::prepGetList( $_REQUEST );
-		if( empty ( $_REQUEST['listInfo']['sort_mode'] ) ) {
-			$_REQUEST['listInfo']['sort_mode'] = 'redeem_date_desc';
+		// pagination sucks for pages like this
+		$_REQUEST['max_records'] = 1000;
+		if( empty ( $_REQUEST['sort_mode'] ) ) {
+			$_REQUEST['sort_mode'] = 'redeem_date_desc';
 		}
-		$sql = "SELECT ccrt.`unique_id`, ccrt.*,cot.*,uu.*
+		BitBase::prepGetList( $_REQUEST );
+
+		$joinSql = '';
+		$selectSql = '';	
+		if( $gBitSystem->isPackageActive( 'stats' ) ) {
+			$joinSql .= " LEFT OUTER JOIN `".BIT_DB_PREFIX."stats_referer_users_map` srum ON (srum.`user_id`=uu.`user_id`) 
+						  LEFT OUTER JOIN `".BIT_DB_PREFIX."stats_referer_urls` sru ON (srum.`referer_url_id`=sru.`referer_url_id`)";
+			$selectSql .= ", sru.`referer_url`";
+		}
+		$sql = "SELECT ccrt.`unique_id`, ccrt.`order_id`, ccrt.`redeem_ip`, ccrt.`redeem_date`, cot.`orders_value`, co.`order_total`, uu.`user_id`, uu.`login`, uu.`real_name`, uu.`email`, COUNT(co.`orders_id`) AS `previous_orders`, `redeem_date` - MIN(co.`date_purchased`) AS `customers_age` $selectSql
 				FROM " . TABLE_COUPON_REDEEM_TRACK . " ccrt
 					LEFT OUTER JOIN `".BIT_DB_PREFIX."users_users` uu ON (ccrt.`customer_id`=uu.`user_id`)
+					LEFT OUTER JOIN " . TABLE_ORDERS . " co ON (uu.`user_id`=co.`customers_id` AND co.`date_purchased`<=ccrt.`redeem_date`)
 					LEFT OUTER JOIN " . TABLE_COUPONS . " cc ON (ccrt.`coupon_id`=cc.`coupon_id`)
 					LEFT OUTER JOIN " . TABLE_ORDERS_TOTAL . " cot ON (ccrt.`order_id`=cot.`orders_id` AND cot.`class`='ot_coupon' AND UPPER(cot.`title`) LIKE '%'||UPPER(cc.`coupon_code`)||'%')
+					$joinSql
 				WHERE ccrt.`coupon_id` = ?
-				ORDER BY ".$gBitDb->convertSortmode( $_REQUEST['listInfo']['sort_mode'] );
+				GROUP BY ccrt.`unique_id`, ccrt.`order_id`, ccrt.`redeem_ip`, ccrt.`redeem_date`, cot.`orders_value`, co.`order_total`, uu.`user_id`, uu.`login`, uu.`real_name`, uu.`email` $selectSql
+				ORDER BY ".$gBitDb->convertSortmode( $_REQUEST['sort_mode'] );
 		$bindVars = array( $_REQUEST['cid'] );
 
 		if( empty( $_REQUEST['page'] ) ) {
@@ -92,6 +105,9 @@
 		$_REQUEST['offset'] = ($_REQUEST['page'] ? (($_REQUEST['page'] -1) * $_REQUEST['max_records']) : 0);
 		if( $rs = $gBitDb->query( $sql, $bindVars, $_REQUEST['max_records'], $_REQUEST['offset'] ) ) {
 			while( $row = $rs->fetchRow() ) {
+				$orderHistory = $gBitDb->getRow( "SELECT COUNT(`orders_id`) AS `previous_orders`, MIN(`date_purchased`) AS `first_purchase_date` FROM " . TABLE_ORDERS . " WHERE `customers_id`=? AND `date_purchased` <= ?", array( $row['user_id'], $row['redeem_date'] ) ); 
+				$row['previous_orders'] = $orderHistory['previous_orders'];
+				$row['customers_age'] = substr( $row['customers_age'], 0, strrpos( $row['customers_age'], ' ' ));
 				$redeemList[$row['unique_id']] = $row;
 			}
 			$_REQUEST['listInfo']['page_records'] = $rs->RecordCount();
--- templates/admin_coupon_report.tpl
+++ templates/admin_coupon_report.tpl
@@ -14,18 +14,22 @@
 		<tr >
 			<th>&nbsp;</th>
 			<th>{smartlink ititle="Order" isort="order_id" icontrol=$listInfo }</th>
-			<th>{smartlink ititle="Customer Name" isort="real_name" icontrol=$listInfo}</th>
-			<th>{tr}IP Address{/tr}</th>
-			<th>{smartlink ititle="Redemption" isort="orders_value" icontrol=$listInfo}</th>
+			<th>{smartlink ititle="Customer Name" isort="real_name" icontrol=$listInfo} / {smartlink ititle="Referrer" isort="referer_url" icontrol=$listInfo}</th>
+			<th>{smartlink ititle="IP" isort="redeem_ip" icontrol=$listInfo}</th>
+			<th>{smartlink ititle="Order History" isort="previous_orders" icontrol=$listInfo}</th>
+			<th>{smartlink ititle="Redeemed" isort="orders_value" icontrol=$listInfo}</th>
+			<th>{smartlink ititle="Gross" isort="order_total" icontrol=$listInfo}</th>
 			<th>{smartlink ititle="Date Redeemed" isort="redeem_date" icontrol=$listInfo}</th>
 		</tr>
 		{foreach from=$redeemList item=redeem name=redeemList}
 		<tr>
 			<td class="item">{$smarty.foreach.redeemList.iteration+$listInfo.offset}</td>
 			<td class="item"><a href="{$smarty.const.BITCOMMERCE_PKG_URL}admin/orders.php?oID={$redeem.order_id}">{$redeem.order_id}</a></td>
-			<td class="item">{displayname hash=$redeem}</td>
+			<td class="item"><strong>{displayname hash=$redeem}</strong>{if $redeem.referer_url}<br/><a href="{$redeem.referer_url}">{$redeem.referer_url|stats_referer_display_short|escape}</a>{/if}</td>
 			<td class="item">{$redeem.redeem_ip}</td>
-			<td class="item currency">{$gCommerceCurrencies->format($redeem.orders_value)}</td>
+			<td class="item"><a href="list_orders.php?user_id={$redeem.user_id}&amp;orders_status_id=all&amp;list_filter=all">{$redeem.previous_orders|ordinalize}</a>{if $redeem.customers_age} {tr}in{/tr} {$redeem.customers_age}{/if}</td>
+			<td class="item currency">{$gCommerceCurrencies->format($redeem.orders_value)} {tr}off{/tr}</td>
+			<td class="item currency">{$gCommerceCurrencies->format($redeem.order_total+$redeem.orders_value)} </td>
 			<td class="item">{$redeem.redeem_date|strtotime|bit_short_datetime}</td>
 		</tr>
 		{/foreach}
