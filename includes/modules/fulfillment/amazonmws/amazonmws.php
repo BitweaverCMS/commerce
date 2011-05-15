050cf6517355e05eb54b217fa4c4a81a419c1ead
Christian Fowler <spider@viovio.com>
2011-05-15 13:40:51 -0400

    finish up amazonmws integration

--- includes/modules/fulfillment/amazonmws/amazonmws.php
+++ includes/modules/fulfillment/amazonmws/amazonmws.php
@@ -1,11 +1,13 @@
 <?php
-// +---------------------------------------------------------------------------+
-// | Copyright (c) 2004-2007 viovio.com, Proprietary CODE, ALL RIGHTS RESERVED |
-// +---------------------------------------------------------------------------+
-// $Id: amazonmws.php,v 1.7 2010/04/02 15:45:49 cfowler Exp $
-//
-
-// Note this is temporary
+// +--------------------------------------------------------------------+
+// | bitcommerce														|
+// +--------------------------------------------------------------------+
+// | Copyright (c) 2011 bitcommerce.org									|
+// |																	|
+// | http://www.bitcommerce.org											|
+// +--------------------------------------------------------------------+
+// | This source file is subject to version 2.0 of the GPL license		|
+// +--------------------------------------------------------------------+
 
 class amazonmws extends BitBase { 
    var $code;
@@ -81,17 +83,27 @@ class amazonmws extends BitBase {
 	}
 
    function install() {
-     global $gBitDb;
+     global $gBitDb, $gBitUser;
      $gBitDb->StartTrans();
      $gBitDb->Execute("insert into " . TABLE_CONFIGURATION . " (`configuration_title`, `configuration_key`, `configuration_value`, `configuration_description`, `configuration_group_id`, `sort_order`, `set_function`, `date_added`) values ('Enable AmazonMWS Module', 'MODULE_FULFILLMENT_AMAZONMWS_STATUS', 'True', 'Do you want enable AmazonMWS fulfillment?', '6', '0', 'zen_cfg_select_option(array(\'True\', \'False\'), ', now())");
      $gBitDb->Execute("insert into " . TABLE_CONFIGURATION . " (`configuration_title`, `configuration_key`, `configuration_value`, `configuration_description`, `configuration_group_id`, `sort_order`, `set_function`, `date_added`) values ('AmazonMWS Activation Mode', 'MODULE_FULFILLMENT_AMAZONMWS_MODE', 'Test', 'What mode is your account in?', '6', '2', 'zen_cfg_select_option(array(\'Production\', \'Test\'), ', now())");
+     $gBitDb->Execute("insert into " . TABLE_CONFIGURATION . " (`configuration_title`, `configuration_key`, `configuration_value`, `configuration_description`, `configuration_group_id`, `sort_order`, `date_added`) values ('Local Username', 'MODULE_FULFILLMENT_AMAZONMWS_LOCAL_USERNAME','amazonmws', 'This is the username on this site under which all orders will be processed.', '6', '4', now())");
      $gBitDb->Execute("insert into " . TABLE_CONFIGURATION . " (`configuration_title`, `configuration_key`, `configuration_value`, `configuration_description`, `configuration_group_id`, `sort_order`, `date_added`) values ('Merchant ID', 'MODULE_FULFILLMENT_AMAZONMWS_MERCHANT_ID','', '', '6', '4', now())");
      $gBitDb->Execute("insert into " . TABLE_CONFIGURATION . " (`configuration_title`, `configuration_key`, `configuration_value`, `configuration_description`, `configuration_group_id`, `sort_order`, `date_added`) values ('Marketplace ID', 'MODULE_FULFILLMENT_AMAZONMWS_MARKETPLACE_ID','', '', '6', '4', now())");
      $gBitDb->Execute("insert into " . TABLE_CONFIGURATION . " (`configuration_title`, `configuration_key`, `configuration_value`, `configuration_description`, `configuration_group_id`, `sort_order`, `date_added`) values ('AWS Access Key ID', 'MODULE_FULFILLMENT_AMAZONMWS_AWS_ACCESS_KEY_ID','', '', '6', '4', now())");
      $gBitDb->Execute("insert into " . TABLE_CONFIGURATION . " (`configuration_title`, `configuration_key`, `configuration_value`, `configuration_description`, `configuration_group_id`, `sort_order`, `date_added`) values ('Secret Key', 'MODULE_FULFILLMENT_AMAZONMWS_SECRET_KEY','', '', '6', '4', now())");
-    $gBitDb->Execute("insert into " . TABLE_CONFIGURATION . " (`configuration_title`, `configuration_key`, `configuration_value`, `configuration_description`, `configuration_group_id`, `sort_order`, `set_function`, `use_function`, `date_added`) values ('Initial Order Status', 'MODULE_FULFILLMENT_AMAZONMWS_INITIAL_ORDER_STATUS_ID', '30', 'Orders with this status will be processed for fulfillment<br />(\'Transferred\' recommended)', '6', '5', 'zen_cfg_pull_down_order_statuses(', 'zen_get_order_status_name', now())");
-    $gBitDb->Execute("insert into " . TABLE_CONFIGURATION . " (`configuration_title`, `configuration_key`, `configuration_value`, `configuration_description`, `configuration_group_id`, `sort_order`, `set_function`, `use_function`, `date_added`) values ('Final Order Status', 'MODULE_FULFILLMENT_AMAZONMWS_FINAL_ORDER_STATUS_ID', '40', 'Set the status of orders that have completed fulfillment to this value<br />(\'Fulfilling\' recommended)', '6', '5', 'zen_cfg_pull_down_order_statuses(', 'zen_get_order_status_name', now())");
-     $gBitDb->Execute("insert into " . TABLE_CONFIGURATION . " (`configuration_title`, `configuration_key`, `configuration_value`, `configuration_description`, `configuration_group_id`, `sort_order`, `date_added`) values ('Manufacturing Days', 'MODULE_FULFILLMENT_AMAZONMWS_MANF_TIME','3', 'Number of days it takes to manufacture books', '6', '8', now())");
+    $gBitDb->Execute("insert into " . TABLE_CONFIGURATION . " (`configuration_title`, `configuration_key`, `configuration_value`, `configuration_description`, `configuration_group_id`, `sort_order`, `set_function`, `use_function`, `date_added`) values ('Initial Order Status', 'MODULE_FULFILLMENT_AMAZONMWS_INITIAL_ORDER_STATUS_ID', '20', 'Orders with this status will be processed for fulfillment<br />(\'Transferred\' recommended)', '6', '5', 'zen_cfg_pull_down_order_statuses(', 'zen_get_order_status_name', now())");
+//	$gBitDb->Execute("insert into " . TABLE_CONFIGURATION . " (`configuration_title`, `configuration_key`, `configuration_value`, `configuration_description`, `configuration_group_id`, `sort_order`, `set_function`, `use_function`, `date_added`) values ('Final Order Status', 'MODULE_FULFILLMENT_AMAZONMWS_FINAL_ORDER_STATUS_ID', '40', 'Set the status of orders that have completed fulfillment to this value<br />(\'Fulfilling\' recommended)', '6', '5', 'zen_cfg_pull_down_order_statuses(', 'zen_get_order_status_name', now())");
+//	$gBitDb->Execute("insert into " . TABLE_CONFIGURATION . " (`configuration_title`, `configuration_key`, `configuration_value`, `configuration_description`, `configuration_group_id`, `sort_order`, `date_added`) values ('Manufacturing Days', 'MODULE_FULFILLMENT_AMAZONMWS_MANF_TIME','3', 'Number of days it takes to manufacture books', '6', '8', now())");
+
+	if( !$gBitUser->lookupHomepage( 'amazonmws' ) ) {
+		$newUser = new BitPermUser();
+		$userHash['login'] = 'amazonmws';
+		$userHash['email'] = str_replace( '@', '+amazonmws@', STORE_OWNER_EMAIL_ADDRESS );
+		$userHash['real_name'] = 'Amazon Marketplace';
+		$userHash['hash'] = $gBitUser->getField( 'hash' );
+		$newUser->importUser( $userHash );
+	}
     $gBitDb->CompleteTrans();
 }
 
@@ -104,6 +116,7 @@ class amazonmws extends BitBase {
      return array(
 		'MODULE_FULFILLMENT_AMAZONMWS_STATUS',
 		'MODULE_FULFILLMENT_AMAZONMWS_MODE',
+		'MODULE_FULFILLMENT_AMAZONMWS_LOCAL_USERNAME',
 		'MODULE_FULFILLMENT_AMAZONMWS_MERCHANT_ID',
 		'MODULE_FULFILLMENT_AMAZONMWS_MARKETPLACE_ID',
 		'MODULE_FULFILLMENT_AMAZONMWS_AWS_ACCESS_KEY_ID',		
