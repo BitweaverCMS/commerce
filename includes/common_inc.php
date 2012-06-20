82ca0d8e5c3015b0f7454ede76b0bb6075ff3513
spiderr <spider@viovio.com>
2012-06-20 12:59:41 -0400

    nuke zen_round stupidity

--- includes/common_inc.php
+++ includes/common_inc.php
@@ -1050,23 +1050,7 @@ function reset_bitcommerce_layout() {
 ////
 // Wrapper function for round()
   function zen_round($number, $precision) {
-    if (strpos($number, '.') && (strlen(substr($number, strpos($number, '.')+1)) > $precision)) {
-      $number = substr($number, 0, strpos($number, '.') + 1 + $precision + 1);
-
-      if (substr($number, -1) >= 5) {
-        if ($precision > 1) {
-          $number = substr($number, 0, -1) + ('0.' . str_repeat(0, $precision-1) . '1');
-        } elseif ($precision == 1) {
-          $number = substr($number, 0, -1) + 0.1;
-        } else {
-          $number = substr($number, 0, -1) + 1;
-        }
-      } else {
-        $number = substr($number, 0, -1);
-      }
-    }
-
-    return $number;
+	return round( $number, $precision );
   }
 
 
