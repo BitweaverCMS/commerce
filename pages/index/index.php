0f4aad6dc53ed1c8b3acc87e74fc71767c56d2e3
spiderr <spider@viovio.com>
2013-03-04 03:09:58 -0500

    revert removal of header_php.php

--- pages/index/index.php
+++ pages/index/index.php
@@ -18,27 +18,6 @@
 // | license@zen-cart.com so we can mail you a copy immediately.          |
 // +----------------------------------------------------------------------+
 // $Id$
-//
-//die($category_depth);
-//die($_REQUEST['music_genre_id']);
-
-// the following cPath references come from application_top.php
-  $category_depth = 'top';
-  if (isset($cPath) && zen_not_null($cPath)) {
-    if ( $gComCategory->countProductsInCategory( $current_category_id ) ) {
-      $category_depth = 'products'; // display products
-    } else {
-      if ( $gComCategory->countParentCategories( $current_category_id ) ) {
-        $category_depth = 'nested'; // navigate through the categories
-      } else {
-        $category_depth = 'products'; // category has no products, but display the 'no products' message
-      }
-    }
-  }
-// include template specific file name defines
-  $define_main_page = zen_get_file_directory(DIR_WS_LANGUAGES . $gBitCustomer->getLanguage() . '/html_includes/', FILENAME_DEFINE_MAIN_PAGE, 'false');
-  require_once(DIR_FS_MODULES . 'require_languages.php');
-
 
 // release manufactures_id when nothing is there so a blank filter is not setup.
 // this will result in the home page, if used
