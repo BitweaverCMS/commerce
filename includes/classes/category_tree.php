104052208db4c4c23b4ba88ba19aae60be5dc62a
turon <spider@turon>
2016-02-18 15:34:32 -0500

    clean up zen_category_tree query

--- includes/classes/category_tree.php
+++ includes/classes/category_tree.php
@@ -20,175 +20,169 @@
 // $Id$
 //
 
-  class category_tree {
+class category_tree {
 
-    function zen_category_tree($product_type = "all") {
-      global $gBitDb, $cPath, $cPath_array;
-      if ($product_type != 'all') {
-        $sql = "select type_master_type from " . TABLE_PRODUCT_TYPES . "
-                where type_master_type = '" . $product_type . "'";
-        $master_type_result = $gBitDb->Execute($sql);
-        $master_type = $master_type_result->fields['type_master_type'];
-      }
-      $this->tree = array();
-		$bindVars = array( (int)$_SESSION['languages_id'] );
-      if ($product_type == 'all') {
-        $categories_query = "select c.`categories_id`, cd.`categories_name`, c.`parent_id`
-                             from " . TABLE_CATEGORIES . " c, " . TABLE_CATEGORIES_DESCRIPTION . " cd
-                             where c.`parent_id` = '0'
-                             and c.`categories_id` = cd.`categories_id`
-                             and cd.`language_id`=?
-                             and c.`categories_status`= '1'
-                             order by `sort_order`, cd.`categories_name`";
-      } else {
-        $categories_query = "select ptc.`category_id` as `categories_id`, cd.`categories_name`, c.`parent_id`
-                             from " . TABLE_CATEGORIES . " c, " . TABLE_CATEGORIES_DESCRIPTION . " cd, " . TABLE_PRODUCT_TYPES_TO_CATEGORY . " ptc
-                             where c.`parent_id` = '0'
-                             and ptc.`category_id` = cd.`categories_id`
-                             and c.`categories_id` = ptc.`category_id`
-                             and cd.`language_id`=?
-                             and ptc.`product_type_id` = ?
-                             and c.`categories_status`= '1'
-                             order by `sort_order`, cd.`categories_name`";
-		$bindVars[] = $master_type;
-      }
-      $categories = $gBitDb->Execute($categories_query, $bindVars, true, 150);
-      while (!$categories->EOF)  {
-        $this->tree[$categories->fields['categories_id']] = array('name' => $categories->fields['categories_name'],
-                                                    'parent' => $categories->fields['parent_id'],
-                                                    'level' => 0,
-                                                    'path' => $categories->fields['categories_id'],
-                                                    'next_id' => false);
+	function zen_category_tree($product_type = "all") {
+		global $gBitDb, $cPath, $cPath_array;
+		if ($product_type != 'all') {
+			$sql = "select type_master_type from " . TABLE_PRODUCT_TYPES . "
+							where type_master_type = '" . $product_type . "'";
+			$master_type_result = $gBitDb->Execute($sql);
+			$master_type = $master_type_result->fields['type_master_type'];
+		}
+		$this->tree = array();
+		$bindVars = array( $_SESSION['languages_id'] );
+		if ($product_type == 'all') {
+			$categories_query = "SELECT c.`categories_id`, cd.`categories_name`, c.`parent_id`
+								 FROM " . TABLE_CATEGORIES . " c
+									INNER JOIN " . TABLE_CATEGORIES_DESCRIPTION . " cd ON (c.`categories_id` = cd.`categories_id`)
+								 WHERE c.`parent_id` = '0' AND c.`categories_status`= '1' AND cd.`language_id`=?
+								 ORDER BY `sort_order`, cd.`categories_name`";
+		} else {
+			$categories_query = "SELECT ptc.`category_id` as `categories_id`, cd.`categories_name`, c.`parent_id`
+								 FROM " . TABLE_CATEGORIES . " c
+									  INNER JOIN " . TABLE_CATEGORIES_DESCRIPTION . " cd ON (ptc.`category_id` = cd.`categories_id`)
+									  INNER JOIN " . TABLE_PRODUCT_TYPES_TO_CATEGORY . " ptc ON (c.`categories_id` = ptc.`category_id`)
+								 WHERE c.`parent_id` = '0' AND c.`categories_status`= '1' AND cd.`language_id`=?  AND ptc.`product_type_id`=?
+								 ORDER BY `sort_order`, cd.`categories_name`";
+			$bindVars[] = $master_type;
+		}
+		$categories = $gBitDb->query($categories_query, $bindVars, true, 150);
+		while (!$categories->EOF)	{
+			$this->tree[$categories->fields['categories_id']] = array('name' => $categories->fields['categories_name'],
+																									'parent' => $categories->fields['parent_id'],
+																									'level' => 0,
+																									'path' => $categories->fields['categories_id'],
+																									'next_id' => false);
 
-        if (isset($parent_id)) {
-          $this->tree[$parent_id]['next_id'] = $categories->fields['categories_id'];
-        }
+			if (isset($parent_id)) {
+				$this->tree[$parent_id]['next_id'] = $categories->fields['categories_id'];
+			}
 
-        $parent_id = $categories->fields['categories_id'];
+			$parent_id = $categories->fields['categories_id'];
 
-        if (!isset($first_element)) {
-          $first_element = $categories->fields['categories_id'];
-        }
-        $categories->MoveNext();
-      }
-      if (zen_not_null($cPath)) {
-        $new_path = '';
-        reset($cPath_array);
-        while (list($key, $value) = each($cPath_array)) {
-          unset($parent_id);
-          unset($first_id);
-      if ($product_type == 'all') {
-          $categories_query = "select c.`categories_id`, cd.`categories_name`, c.`parent_id`
-                               from " . TABLE_CATEGORIES . " c, " . TABLE_CATEGORIES_DESCRIPTION . " cd
-                               where c.`parent_id` = '" . (int)$value . "'
-                               and c.`categories_id` = cd.`categories_id`
-                               and cd.`language_id`='" . (int)$_SESSION['languages_id'] . "'
-                               and c.`categories_status`= '1'
-                               order by `sort_order`, cd.`categories_name`";
-     } else {
+			if (!isset($first_element)) {
+				$first_element = $categories->fields['categories_id'];
+			}
+			$categories->MoveNext();
+		}
+		if (zen_not_null($cPath)) {
+			$new_path = '';
+			reset($cPath_array);
+			while (list($key, $value) = each($cPath_array)) {
+				unset($parent_id);
+				unset($first_id);
+		if ($product_type == 'all') {
+				$categories_query = "select c.`categories_id`, cd.`categories_name`, c.`parent_id`
+														 from " . TABLE_CATEGORIES . " c, " . TABLE_CATEGORIES_DESCRIPTION . " cd
+														 where c.`parent_id` = '" . (int)$value . "'
+														 and c.`categories_id` = cd.`categories_id`
+														 and cd.`language_id`='" . (int)$_SESSION['languages_id'] . "'
+														 and c.`categories_status`= '1'
+														 order by `sort_order`, cd.`categories_name`";
+	 } else {
 /*
-          $categories_query = "select ptc.`category_id` as categories, cd.`categories_name`, c.`parent_id`
-                               from " . TABLE_CATEGORIES . " c, " . TABLE_CATEGORIES_DESCRIPTION . " cd, " . TABLE_PRODUCT_TYPES_TO_CATEGORY . " ptc
-                               where c.`parent_id` = '" . (int)$value . "'
-                               and ptc.`category_id` = cd.`categories_id`
-                             and ptc.`product_type_id` = '" . $master_type . "'
-                               and cd.`language_id`='" . (int)$_SESSION['languages_id'] . "'
-                               and c.`categories_status`= '1'
-                               order by `sort_order`, cd.`categories_name`";
+				$categories_query = "select ptc.`category_id` as categories, cd.`categories_name`, c.`parent_id`
+														 from " . TABLE_CATEGORIES . " c, " . TABLE_CATEGORIES_DESCRIPTION . " cd, " . TABLE_PRODUCT_TYPES_TO_CATEGORY . " ptc
+														 where c.`parent_id` = '" . (int)$value . "'
+														 and ptc.`category_id` = cd.`categories_id`
+													 and ptc.`product_type_id` = '" . $master_type . "'
+														 and cd.`language_id`='" . (int)$_SESSION['languages_id'] . "'
+														 and c.`categories_status`= '1'
+														 order by `sort_order`, cd.`categories_name`";
 */
-        $categories_query = "select ptc.`category_id` as categories_id, cd.`categories_name`, c.`parent_id`
-                             from " . TABLE_CATEGORIES . " c, " . TABLE_CATEGORIES_DESCRIPTION . " cd, " . TABLE_PRODUCT_TYPES_TO_CATEGORY . " ptc
-                             where c.`parent_id` = '" . (int)$value . "'
-                             and ptc.`category_id` = cd.`categories_id`
-                             and ptc.`product_type_id` = '" . $master_type . "'
-                             and c.`categories_id` = ptc.`category_id`
-                             and cd.`language_id`='" . (int)$_SESSION['languages_id'] ."'
-                             and c.`categories_status`= '1'
-                             order by `sort_order`, cd.`categories_name`";
+			$categories_query = "select ptc.`category_id` as categories_id, cd.`categories_name`, c.`parent_id`
+													 from " . TABLE_CATEGORIES . " c, " . TABLE_CATEGORIES_DESCRIPTION . " cd, " . TABLE_PRODUCT_TYPES_TO_CATEGORY . " ptc
+													 where c.`parent_id` = '" . (int)$value . "'
+													 and ptc.`category_id` = cd.`categories_id`
+													 and ptc.`product_type_id` = '" . $master_type . "'
+													 and c.`categories_id` = ptc.`category_id`
+													 and cd.`language_id`='" . (int)$_SESSION['languages_id'] ."'
+													 and c.`categories_status`= '1'
+													 order by `sort_order`, cd.`categories_name`";
 
-     }
+	 }
 
-          $rows = $gBitDb->Execute($categories_query);
+				$rows = $gBitDb->Execute($categories_query);
 
-          if ($rows->RecordCount()>0) {
-            $new_path .= $value;
-            while (!$rows->EOF) {
-              $this->tree[$rows->fields['categories_id']] = array('name' => $rows->fields['categories_name'],
-                                                   'parent' => $rows->fields['parent_id'],
-                                                   'level' => $key+1,
-                                                   'path' => $new_path . '_' . $rows->fields['categories_id'],
-                                                   'next_id' => false);
+				if ($rows->RecordCount()>0) {
+					$new_path .= $value;
+					while (!$rows->EOF) {
+						$this->tree[$rows->fields['categories_id']] = array('name' => $rows->fields['categories_name'],
+																								 'parent' => $rows->fields['parent_id'],
+																								 'level' => $key+1,
+																								 'path' => $new_path . '_' . $rows->fields['categories_id'],
+																								 'next_id' => false);
 
-              if (isset($parent_id)) {
-                $this->tree[$parent_id]['next_id'] = $rows->fields['categories_id'];
-              }
+						if (isset($parent_id)) {
+							$this->tree[$parent_id]['next_id'] = $rows->fields['categories_id'];
+						}
 
-              $parent_id = $rows->fields['categories_id'];
-              if (!isset($first_id)) {
-                $first_id = $rows->fields['categories_id'];
-              }
+						$parent_id = $rows->fields['categories_id'];
+						if (!isset($first_id)) {
+							$first_id = $rows->fields['categories_id'];
+						}
 
-              $last_id = $rows->fields['categories_id'];
-            $rows->MoveNext();
-            }
-            $this->tree[$last_id]['next_id'] = $this->tree[$value]['next_id'];
-            $this->tree[$value]['next_id'] = $first_id;
-            $new_path .= '_';
-          } else {
-            break;
-         }
-       }
-     }
-     $row = 0;
-     return $this->zen_show_category($first_element, $row);
-    }
+						$last_id = $rows->fields['categories_id'];
+					$rows->MoveNext();
+					}
+					$this->tree[$last_id]['next_id'] = $this->tree[$value]['next_id'];
+					$this->tree[$value]['next_id'] = $first_id;
+					$new_path .= '_';
+				} else {
+					break;
+			 }
+		 }
+	 }
+	 $row = 0;
+	 return $this->zen_show_category($first_element, $row);
+	}
 
-    function zen_show_category($counter,$ii) {
-      global $cPath_array;
+	function zen_show_category($counter,$ii) {
+		global $cPath_array;
 
-      $this->categories_string = "";
+		$this->categories_string = "";
 
-      for ($i=0; $i<$this->tree[$counter]['level']; $i++) {
-        if ($this->tree[$counter]['parent'] != 0) {
-          $this->categories_string .= CATEGORIES_SUBCATEGORIES_INDENT;
-        }
-      }
+		for ($i=0; $i<$this->tree[$counter]['level']; $i++) {
+			if ($this->tree[$counter]['parent'] != 0) {
+				$this->categories_string .= CATEGORIES_SUBCATEGORIES_INDENT;
+			}
+		}
 
 
-      if ($this->tree[$counter]['parent'] == 0) {
-        $cPath_new = 'cPath=' . $counter;
-        $this->box_categories_array[$ii]['top'] = 'true';
-      } else {
-        $this->box_categories_array[$ii]['top'] = 'false';
-        $cPath_new = 'cPath=' . $this->tree[$counter]['path'];
-        $this->categories_string .= CATEGORIES_SEPARATOR_SUBS;
-      }
-      $this->box_categories_array[$ii]['path'] = $cPath_new;
+		if ($this->tree[$counter]['parent'] == 0) {
+			$cPath_new = 'cPath=' . $counter;
+			$this->box_categories_array[$ii]['top'] = 'true';
+		} else {
+			$this->box_categories_array[$ii]['top'] = 'false';
+			$cPath_new = 'cPath=' . $this->tree[$counter]['path'];
+			$this->categories_string .= CATEGORIES_SEPARATOR_SUBS;
+		}
+		$this->box_categories_array[$ii]['path'] = $cPath_new;
 
-      if (isset($cPath_array) && in_array($counter, $cPath_array)) {
-        $this->box_categories_array[$ii]['current'] = true;
-      }
+		if (isset($cPath_array) && in_array($counter, $cPath_array)) {
+			$this->box_categories_array[$ii]['current'] = true;
+		}
 
 // display category name
-      $this->box_categories_array[$ii]['name'] = $this->categories_string . $this->tree[$counter]['name'];
+		$this->box_categories_array[$ii]['name'] = $this->categories_string . $this->tree[$counter]['name'];
 
 
-      if (zen_has_category_subcategories($counter)) {
-        $this->box_categories_array[$ii]['has_sub_cat'] = true;
-      }
+		if (zen_has_category_subcategories($counter)) {
+			$this->box_categories_array[$ii]['has_sub_cat'] = true;
+		}
 
-      if (SHOW_COUNTS == 'true') {
-        $products_in_category = zen_count_products_in_category($counter);
-        if ($products_in_category > 0) {
-          $this->box_categories_array[$ii]['count'] = $products_in_category;
-        }
-      }
+		if (SHOW_COUNTS == 'true') {
+			$products_in_category = zen_count_products_in_category($counter);
+			if ($products_in_category > 0) {
+				$this->box_categories_array[$ii]['count'] = $products_in_category;
+			}
+		}
 
-      if ($this->tree[$counter]['next_id'] != false) {
-        $ii++;
-        $this->zen_show_category($this->tree[$counter]['next_id'], $ii);
-      }
-      return $this->box_categories_array;
-    }
-  }
-?>
+		if ($this->tree[$counter]['next_id'] != false) {
+			$ii++;
+			$this->zen_show_category($this->tree[$counter]['next_id'], $ii);
+		}
+		return $this->box_categories_array;
+	}
+}
