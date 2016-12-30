2f0b0a34b7e5a2caf09fc112703b358e6ae5d962
spider@app1 <spiderr@bitweaver.org>
2016-12-30 11:35:32 -0500

    change checkout in cart page to update cart too

--- pages/shopping_cart/shopping_cart.php
+++ pages/shopping_cart/shopping_cart.php
@@ -8,7 +8,7 @@
 
 global $gBitSmarty;
 
-if( !empty( $_REQUEST['update_cart'] ) ) {
+if( !empty( $_REQUEST['update_cart'] ) || !empty( $_REQUEST['checkout'] ) ) {
 	// customer wants to update the product quantity in the shopping cart, empty quantity will remove it from the cart
 	foreach( array_keys( $_REQUEST['cart_quantity'] ) as $productsKey ) {
 		$gBitCustomer->mCart->updateQuantity( $productsKey, $_REQUEST['cart_quantity'][$productsKey] );
@@ -20,6 +20,10 @@ if( !empty( $_REQUEST['update_cart'] ) ) {
 			$gBitCustomer->mCart->updateQuantity( $productsKey, 0 );
 		}
 	}
+
+	if( !empty( $_REQUEST['checkout'] ) ) {
+		zen_redirect(zen_href_link('checkout_proof'));
+	}
 } elseif( !empty( $_REQUEST['remove_product'] ) ) {
 	$gBitCustomer->mCart->updateQuantity( $_REQUEST['remove_product'], 0 );
 }
@@ -27,4 +31,3 @@ if( !empty( $_REQUEST['update_cart'] ) ) {
 $gBitSmarty->assign_by_ref( 'gBitCustomer', $gBitCustomer );
 $gBitSmarty->display( 'bitpackage:bitcommerce/page_shopping_cart.tpl' );
 
-?>
