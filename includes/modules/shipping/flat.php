90ec5f55d390e2cded1a631bb18009078314deed
spider@dev <spiderr@bitweaver.org>
2017-01-16 12:57:29 -0500

    clean flat shipping method to use base class, clean up global crap in constructor

--- includes/modules/shipping/flat.php
+++ includes/modules/shipping/flat.php
@@ -1,60 +1,30 @@
 <?php
-//
 // +----------------------------------------------------------------------+
-// |zen-cart Open Source E-commerce																			 |
+// | bitcommerce,	http://www.bitcommerce.org                            |
 // +----------------------------------------------------------------------+
-// | Copyright (c) 2004 The zen-cart developers													 |
-// |																																			|
-// | http://www.zen-cart.com/index.php																		|
-// |																																			|
-// | Portions Copyright (c) 2003 osCommerce															 |
+// | Copyright (c) 2017 bitcommerce.org                                   |
+// | This source file is subject to version 3.0 of the GPL license        |
+// | Portions Copyrigth (c) 2005 http://www.zen-cart.com                  |
+// | Portions Copyright (c) 2003 osCommerce                               |
 // +----------------------------------------------------------------------+
-// | This source file is subject to version 2.0 of the GPL license,			 |
-// | that is bundled with this package in the file LICENSE, and is				|
-// | available through the world-wide-web at the following url:					 |
-// | http://www.zen-cart.com/license/2_0.txt.														 |
-// | If you did not receive a copy of the zen-cart license and are unable |
-// | to obtain it through the world-wide-web, please send a note to			 |
-// | license@zen-cart.com so we can mail you a copy immediately.					|
-// +----------------------------------------------------------------------+
-// $Id$
-//
 
-class flat {
-	var $code, $title, $description, $icon, $enabled;
+require_once( BITCOMMERCE_PKG_PATH.'classes/CommercePluginShippingBase.php' );
+
+class flat extends CommercePluginShippingBase {
+	protected $tax_class, $tax_basis;
 
 	// class constructor
 	function __construct() {
-		global $order, $gBitDb;
 
-		$this->code = 'flat';
-	if( defined( 'MODULE_SHIPPING_FLAT_STATUS' ) ) {
-		$this->title = MODULE_SHIPPING_FLAT_TEXT_TITLE;
-		$this->description = MODULE_SHIPPING_FLAT_TEXT_DESCRIPTION;
-		$this->sort_order = MODULE_SHIPPING_FLAT_SORT_ORDER;
+		parent::__construct();
+
+		$this->title = tra( 'Flat Rate' );
+		$this->description = tra( 'Fixed price shipping for orders of any size.' );
 		$this->icon = '';
-		$this->tax_class = MODULE_SHIPPING_FLAT_TAX_CLASS;
-		$this->tax_basis = MODULE_SHIPPING_FLAT_TAX_BASIS;
-		$this->enabled = ((MODULE_SHIPPING_FLAT_STATUS == 'True') ? true : false);
-	}
-
-		if ( ($this->enabled == true) && ((int)MODULE_SHIPPING_FLAT_ZONE > 0) ) {
-			$check_flag = false;
-			$check = $gBitDb->Execute("select `zone_id` from " . TABLE_ZONES_TO_GEO_ZONES . " where `geo_zone_id` = '" . MODULE_SHIPPING_FLAT_ZONE . "' and `zone_country_id` = '" . $order->delivery['country']['countries_id'] . "' order by `zone_id`");
-			while (!$check->EOF) {
-				if ($check->fields['zone_id'] < 1) {
-					$check_flag = true;
-					break;
-				} elseif ($check->fields['zone_id'] == $order->delivery['zone_id']) {
-					$check_flag = true;
-					break;
-				}
-				$check->MoveNext();
-			}
-
-			if ($check_flag == false) {
-				$this->enabled = false;
-			}
+		if( $this->isEnabled() ) {
+			$this->sort_order = MODULE_SHIPPING_FLAT_SORT_ORDER;
+			$this->tax_class = MODULE_SHIPPING_FLAT_TAX_CLASS;
+			$this->tax_basis = MODULE_SHIPPING_FLAT_TAX_BASIS;
 		}
 	}
 
@@ -62,29 +32,40 @@ class flat {
 	function quote( $pShipHash = array() ) {
 		global $order;
 
-		$this->quotes = array('id' => $this->code,
-							'module' => MODULE_SHIPPING_FLAT_TEXT_TITLE,
-							'methods' => array(array('id' => $this->code,
-							'title' => MODULE_SHIPPING_FLAT_TEXT_WAY,
-							'cost' => MODULE_SHIPPING_FLAT_COST)));
-		if ($this->tax_class > 0) {
-			$this->quotes['tax'] = zen_get_tax_rate($this->tax_class, $order->delivery['country']['countries_id'], $order->delivery['zone_id']);
+		$this->quotes = array();
+
+		$quoteOrder = TRUE;
+		if ( ((int)MODULE_SHIPPING_FLAT_ZONE > 0) ) {
+			$quoteOrder = false;
+			$check = $this->mDB->query("select `zone_id` from " . TABLE_ZONES_TO_GEO_ZONES . " where `geo_zone_id` =? and `zone_country_id` = ? order by `zone_id`", array( MODULE_SHIPPING_FLAT_ZONE, $order->delivery['country']['countries_id'] ) );
+			while (!$check->EOF) {
+				if ($check->fields['zone_id'] < 1) {
+					$quoteOrder = true;
+					break;
+				} elseif ($check->fields['zone_id'] == $order->delivery['zone_id']) {
+					$quoteOrder = true;
+					break;
+				}
+				$check->MoveNext();
+			}
 		}
 
-	if (zen_not_null($this->icon)) {
-		$this->quotes['icon'] = $this->icon;
-	}
+		if( $quoteOrder ) {
+			$this->quotes = array(	'id' => $this->code,
+									'module' => $this->title,
+									'methods' => array(array('id' => $this->code,
+									'title' => tra( 'Best Way' ),
+									'cost' => MODULE_SHIPPING_FLAT_COST)));
+			if ($this->tax_class > 0) {
+				$this->quotes['tax'] = zen_get_tax_rate($this->tax_class, $order->delivery['country']['countries_id'], $order->delivery['zone_id']);
+			}
+
+			if (zen_not_null($this->icon)) {
+				$this->quotes['icon'] = $this->icon;
+			}
+		}
 
 		return $this->quotes;
-	}
-
-	function check() {
-		global $gBitDb;
-		if (!isset($this->_check)) {
-			$check_query = $gBitDb->Execute("select `configuration_value` from " . TABLE_CONFIGURATION . " where `configuration_key` = 'MODULE_SHIPPING_FLAT_STATUS'");
-			$this->_check = $check_query->RecordCount();
-		}
-		return $this->_check;
 	}
 
 	function install() {
@@ -95,11 +76,6 @@ class flat {
 		$gBitDb->Execute("insert into " . TABLE_CONFIGURATION . " (`configuration_title`, `configuration_key`, `configuration_value`, `configuration_description`, `configuration_group_id`, `sort_order`, `set_function`, `date_added`) values ('Tax Basis', 'MODULE_SHIPPING_FLAT_TAX_BASIS', 'Shipping', 'On what basis is Shipping Tax calculated. Options are<br />Shipping - Based on customers Shipping Address<br />Billing Based on customers Billing address<br />Store - Based on Store address if Billing/Shipping Zone equals Store zone', '6', '0', 'zen_cfg_select_option(array(''Shipping'', ''Billing'', ''Store''), ', now())");
 		$gBitDb->Execute("insert into " . TABLE_CONFIGURATION . " (`configuration_title`, `configuration_key`, `configuration_value`, `configuration_description`, `configuration_group_id`, `sort_order`, `use_function`, `set_function`, `date_added`) values ('Shipping Zone', 'MODULE_SHIPPING_FLAT_ZONE', '0', 'If a zone is selected, only enable this shipping method for that zone.', '6', '0', 'zen_get_zone_class_title', 'zen_cfg_pull_down_zone_classes(', now())");
 		$gBitDb->Execute("insert into " . TABLE_CONFIGURATION . " (`configuration_title`, `configuration_key`, `configuration_value`, `configuration_description`, `configuration_group_id`, `sort_order`, `date_added`) values ('Sort Order', 'MODULE_SHIPPING_FLAT_SORT_ORDER', '0', 'Sort order of display.', '6', '0', now())");
-	}
-
-	function remove() {
-		global $gBitDb;
-		$gBitDb->Execute("delete from " . TABLE_CONFIGURATION . " where `configuration_key` in ('" . implode("', '", $this->keys()) . "')");
 	}
 
 	function keys() {
