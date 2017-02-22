5b145b9ec209ff276057bc0568fe2111be50da5f
spider@dev <spiderr@bitweaver.org>
2017-02-22 10:54:49 -0500

    clean up admin header

--- admin/includes/header_navigation.php
+++ admin/includes/header_navigation.php
@@ -23,13 +23,13 @@
 			print '<li><a href="' . zen_href_link_admin(FILENAME_CONFIGURATION, 'gID=' . $configuration_groups['cg_id'], 'NONSSL') . '">' . $configuration_groups['cg_title'] . '</a></li>';
 		}
 ?>
-					<li><a href="<?php echo DIR_WS_HTTPS_ADMIN;?>template_select.php">Template Selection</a></li>
-					<li><a href="/kernel/admin/index.php?page=layout&amp;module_package=bitcommerce">Layout Boxes Controller</a></li>
-					<li><a href="layout_controller.php?action=reset_defaults">RESET LAYOUT</a></li>
-					<li><a href="<?php echo DIR_WS_HTTPS_ADMIN;?>banner_manager.php">Banner Manager</a></li>
-					<li><a href="<?php echo DIR_WS_HTTPS_ADMIN;?>admin.php">Admin Settings</a></li>
-					<li><a href="<?php echo DIR_WS_HTTPS_ADMIN;?>store_manager.php">Store Manager</a></li>
-					<li><a href="<?php echo DIR_WS_HTTPS_ADMIN;?>define_pages_editor.php">Define Pages Editor</a></li>
+					<li><a href="<?php echo DIR_WS_HTTPS_ADMIN;?>template_select.php">{tr}Template Selection{/tr}</a></li>
+					<li><a href="/kernel/admin/index.php?page=layout&amp;module_package=bitcommerce">{tr}Layout Boxes Controller{/tr}</a></li>
+					<li><a href="layout_controller.php?action=reset_defaults">{tr}RESET LAYOUT{/tr}</a></li>
+					<li><a href="<?php echo DIR_WS_HTTPS_ADMIN;?>banner_manager.php">{tr}Banner Manager{/tr}</a></li>
+					<li><a href="<?php echo DIR_WS_HTTPS_ADMIN;?>admin.php">{tr}Admin Settings{/tr}</a></li>
+					<li><a href="<?php echo DIR_WS_HTTPS_ADMIN;?>store_manager.php">{tr}Store Manager{/tr}</a></li>
+					<li><a href="<?php echo DIR_WS_HTTPS_ADMIN;?>define_pages_editor.php">{tr}Define Pages Editor{/tr}</a></li>
 				</ul>
 			</li>
 <?php
@@ -62,75 +62,10 @@
 ?>
 				</ul>
 			</li>
-            <li class="dropdown">
-                <a data-toggle="dropdown" class="dropdown-toggle" href="#"><?php print tra('Catalog');?> <b class="caret"></b></a>
-                <ul class="dropdown-menu">
-					<li><a href="<?php echo DIR_WS_HTTPS_ADMIN;?>categories.php">Categories/Products</a></li>
-					<li><a href="<?php echo DIR_WS_HTTPS_ADMIN;?>product_types.php">Product Types</a></li>
-					<li><a href="<?php echo DIR_WS_HTTPS_ADMIN;?>products_options.php">Product Options</a></li>
-					<li><a href="<?php echo DIR_WS_HTTPS_ADMIN;?>products_price_manager.php">Products Price Manager</a></li>
-					<li><a href="<?php echo DIR_WS_HTTPS_ADMIN;?>downloads_manager.php">Downloads Manager</a></li>
-					<li><a href="<?php echo DIR_WS_HTTPS_ADMIN;?>manufacturers.php">Manufacturers</a></li>
-					<li><a href="<?php echo DIR_WS_HTTPS_ADMIN;?>suppliers.php">Suppliers</a></li>
-					<li><a href="<?php echo DIR_WS_HTTPS_ADMIN;?>reviews.php">Reviews</a></li>
-					<li><a href="<?php echo DIR_WS_HTTPS_ADMIN;?>specials.php">Specials</a></li>
-					<li><a href="<?php echo DIR_WS_HTTPS_ADMIN;?>featured.php">Featured Products</a></li>
-					<li><a href="<?php echo DIR_WS_HTTPS_ADMIN;?>salemaker.php">SaleMaker</a></li>
-					<li><a href="<?php echo DIR_WS_HTTPS_ADMIN;?>products_expected.php">Products Expected</a></li>
-					<li><a href="<?php echo DIR_WS_HTTPS_ADMIN;?>record_artists.php">Record Artists</a></li>
-					<li><a href="<?php echo DIR_WS_HTTPS_ADMIN;?>record_company.php">Record Companies</a></li>
-					<li><a href="<?php echo DIR_WS_HTTPS_ADMIN;?>music_genre.php">Music Genre</a></li>
-					<li><a href="<?php echo DIR_WS_HTTPS_ADMIN;?>media_manager.php">Media Manager</a></li>
-					<li><a href="<?php echo DIR_WS_HTTPS_ADMIN;?>media_types.php">Media Types</a></li>
-					<li><a href="<?php echo DIR_WS_HTTPS_ADMIN;?>easypopulate.php">Easy Populate</a></li>
-				</ul>
-			</li>
-            <li class="dropdown">
-                <a data-toggle="dropdown" class="dropdown-toggle" href="#"><?php print tra('Customer');?> <b class="caret"></b></a>
-                <ul class="dropdown-menu">
-					<li><a href="<?php echo DIR_WS_HTTPS_ADMIN;?>customers.php">Customers</a></li>
-					<li><a href="<?php echo DIR_WS_HTTPS_ADMIN;?>interests.php">Interests</a></li>
-					<li><a href="<?php echo DIR_WS_HTTPS_ADMIN;?>orders.php">Orders</a></li>
-					<li><a href="<?php echo DIR_WS_HTTPS_ADMIN;?>coupon_admin.php">Coupon Admin</a></li>
-					<li><a href="<?php echo DIR_WS_HTTPS_ADMIN;?>includes/modules/amazonmws/index.php">Amazon Orders</a></li>
-					<li><a href="<?php echo DIR_WS_HTTPS_ADMIN;?>commissions.php">Commissions</a></li>
-					<li><a href="<?php echo DIR_WS_HTTPS_ADMIN;?>orders_status.php">Orders Status</a></li>
-					<li><a href="<?php echo DIR_WS_HTTPS_ADMIN;?>group_pricing.php">Group Pricing</a></li>
-					<li><a href="<?php echo DIR_WS_HTTPS_ADMIN;?>paypal.php">PayPal IPN</a></li>
-					<li><a href="<?php echo DIR_WS_HTTPS_ADMIN;?>gv_queue.php">Gift Certificates Queue</a></li>
-					<li><a href="<?php echo DIR_WS_HTTPS_ADMIN;?>gv_mail.php">Mail Gift Certificate</a></li>
-					<li><a href="<?php echo DIR_WS_HTTPS_ADMIN;?>gv_sent.php">Gift Certificates sent</a></li>
-					<li><a href="<?php echo DIR_WS_HTTPS_ADMIN;?>mail.php">Send Email</a></li>
-					<li><a href="<?php echo DIR_WS_HTTPS_ADMIN;?>email_welcome.php">Email Welcome</a></li>
-					<li><a href="<?php echo DIR_WS_HTTPS_ADMIN;?>newsletters.php">Newsletter Manager</a></li>
-				</ul>
-			</li>
-
-            <li class="dropdown">
-                <a data-toggle="dropdown" class="dropdown-toggle" href="#"><?php print tra('Locations');?> <b class="caret"></b></a>
-                <ul class="dropdown-menu">
-					<li><a href="<?php echo DIR_WS_HTTPS_ADMIN;?>countries.php">Countries</a></li>
-					<li><a href="<?php echo DIR_WS_HTTPS_ADMIN;?>currencies.php">Currencies</a></li>
-					<li><a href="<?php echo DIR_WS_HTTPS_ADMIN;?>languages.php">Languages</a></li>
-					<li><a href="<?php echo DIR_WS_HTTPS_ADMIN;?>zones.php">Zones</a></li>
-					<li><a href="<?php echo DIR_WS_HTTPS_ADMIN;?>geo_zones.php">Zones Definitions</a></li>
-					<li><a href="<?php echo DIR_WS_HTTPS_ADMIN;?>tax_classes.php">Tax Classes</a></li>
-					<li><a href="<?php echo DIR_WS_HTTPS_ADMIN;?>tax_rates.php">Tax Rates</a></li>
-				</ul>
-			</li>
-
-            <li class="dropdown">
-                <a data-toggle="dropdown" class="dropdown-toggle" href="#"><?php print tra('Reports');?> <b class="caret"></b></a>
-                <ul class="dropdown-menu">
-					<li><a href="<?php echo DIR_WS_HTTPS_ADMIN;?>sales_and_income.php">Sales and Income</a></li>
-					<li><a href="<?php echo DIR_WS_HTTPS_ADMIN;?>stats_products_viewed.php">Products Viewed</a></li>
-					<li><a href="<?php echo DIR_WS_HTTPS_ADMIN;?>stats_products_types.php">Products Sales By Type</a></li>
-					<li><a href="<?php echo DIR_WS_HTTPS_ADMIN;?>stats_products_purchased.php">Products Purchased</a></li>
-					<li><a href="<?php echo DIR_WS_HTTPS_ADMIN;?>stats_customers.php">Customer Orders</a></li>
-					<li><a href="<?php echo DIR_WS_HTTPS_ADMIN;?>stats_products_lowstock.php">Products Low Stock</a></li>
-					<li><a href="<?php echo DIR_WS_HTTPS_ADMIN;?>stats_customers_referrals.php">Customers Referral</a></li>
-				</ul>
-			</li>
+<?php
+			global $gBitSmarty;
+			print $gBitSmarty->fetch('bitpackage:bitcommerce/admin_header_menu_inc.tpl');
+?>
 		</ul>
 
 		<div class="col-md-3 navbar-right hidden-sm">
---
+++ templates/admin_header_menu_inc.tpl
@@ -0,0 +1,70 @@
+
+            <li class="dropdown">
+                <a data-toggle="dropdown" class="dropdown-toggle" href="#">{tr}Catalog{/tr} <b class="caret"></b></a>
+                <ul class="dropdown-menu">
+					<li><a href="{$smarty.const.DIR_WS_HTTPS_ADMIN}categories.php">{tr}Categories/Products{/tr}</a></li>
+					<li><a href="{$smarty.const.DIR_WS_HTTPS_ADMIN}product_types.php">{tr}Product Types{/tr}</a></li>
+					<li><a href="{$smarty.const.DIR_WS_HTTPS_ADMIN}products_options.php">{tr}Product Options{/tr}</a></li>
+					<li><a href="{$smarty.const.DIR_WS_HTTPS_ADMIN}products_price_manager.php">{tr}Products Price Manager{/tr}</a></li>
+					<li><a href="{$smarty.const.DIR_WS_HTTPS_ADMIN}downloads_manager.php">{tr}Downloads Manager{/tr}</a></li>
+					<li><a href="{$smarty.const.DIR_WS_HTTPS_ADMIN}manufacturers.php">{tr}Manufacturers{/tr}</a></li>
+					<li><a href="{$smarty.const.DIR_WS_HTTPS_ADMIN}suppliers.php">{tr}Suppliers{/tr}</a></li>
+					<li><a href="{$smarty.const.DIR_WS_HTTPS_ADMIN}reviews.php">{tr}Reviews{/tr}</a></li>
+					<li><a href="{$smarty.const.DIR_WS_HTTPS_ADMIN}specials.php">{tr}Specials{/tr}</a></li>
+					<li><a href="{$smarty.const.DIR_WS_HTTPS_ADMIN}featured.php">{tr}Featured Products{/tr}</a></li>
+					<li><a href="{$smarty.const.DIR_WS_HTTPS_ADMIN}salemaker.php">{tr}SaleMaker{/tr}</a></li>
+					<li><a href="{$smarty.const.DIR_WS_HTTPS_ADMIN}products_expected.php">{tr}Products Expected{/tr}</a></li>
+					<li><a href="{$smarty.const.DIR_WS_HTTPS_ADMIN}record_artists.php">{tr}Record Artists{/tr}</a></li>
+					<li><a href="{$smarty.const.DIR_WS_HTTPS_ADMIN}record_company.php">{tr}Record Companies{/tr}</a></li>
+					<li><a href="{$smarty.const.DIR_WS_HTTPS_ADMIN}music_genre.php">{tr}Music Genre{/tr}</a></li>
+					<li><a href="{$smarty.const.DIR_WS_HTTPS_ADMIN}media_manager.php">{tr}Media Manager{/tr}</a></li>
+					<li><a href="{$smarty.const.DIR_WS_HTTPS_ADMIN}media_types.php">{tr}Media Types{/tr}</a></li>
+					<li><a href="{$smarty.const.DIR_WS_HTTPS_ADMIN}easypopulate.php">{tr}Easy Populate{/tr}</a></li>
+				</ul>
+			</li>
+            <li class="dropdown">
+                <a data-toggle="dropdown" class="dropdown-toggle" href="#">{tr}Customer{/tr} <b class="caret"></b></a>
+                <ul class="dropdown-menu">
+					<li><a href="{$smarty.const.DIR_WS_HTTPS_ADMIN}customers.php">{tr}Customers{/tr}</a></li>
+					<li><a href="{$smarty.const.DIR_WS_HTTPS_ADMIN}interests.php">{tr}Interests{/tr}</a></li>
+					<li><a href="{$smarty.const.DIR_WS_HTTPS_ADMIN}orders.php">{tr}Orders{/tr}</a></li>
+					<li><a href="{$smarty.const.DIR_WS_HTTPS_ADMIN}coupon_admin.php">{tr}Coupon Admin{/tr}</a></li>
+					<li><a href="{$smarty.const.DIR_WS_HTTPS_ADMIN}includes/modules/amazonmws/index.php">{tr}Amazon Orders{/tr}</a></li>
+					<li><a href="{$smarty.const.DIR_WS_HTTPS_ADMIN}commissions.php">{tr}Commissions{/tr}</a></li>
+					<li><a href="{$smarty.const.DIR_WS_HTTPS_ADMIN}orders_status.php">{tr}Orders Status{/tr}</a></li>
+					<li><a href="{$smarty.const.DIR_WS_HTTPS_ADMIN}group_pricing.php">{tr}Group Pricing{/tr}</a></li>
+					<li><a href="{$smarty.const.DIR_WS_HTTPS_ADMIN}paypal.php">{tr}PayPal IPN{/tr}</a></li>
+					<li><a href="{$smarty.const.DIR_WS_HTTPS_ADMIN}gv_queue.php">{tr}Gift Certificates Queue{/tr}</a></li>
+					<li><a href="{$smarty.const.DIR_WS_HTTPS_ADMIN}gv_mail.php">{tr}Mail Gift Certificate{/tr}</a></li>
+					<li><a href="{$smarty.const.DIR_WS_HTTPS_ADMIN}gv_sent.php">{tr}Gift Certificates sent{/tr}</a></li>
+					<li><a href="{$smarty.const.DIR_WS_HTTPS_ADMIN}mail.php">{tr}Send Email{/tr}</a></li>
+					<li><a href="{$smarty.const.DIR_WS_HTTPS_ADMIN}email_welcome.php">{tr}Email Welcome{/tr}</a></li>
+					<li><a href="{$smarty.const.DIR_WS_HTTPS_ADMIN}newsletters.php">{tr}Newsletter Manager{/tr}</a></li>
+				</ul>
+			</li>
+
+            <li class="dropdown">
+                <a data-toggle="dropdown" class="dropdown-toggle" href="#">{tr}Locations{/tr} <b class="caret"></b></a>
+                <ul class="dropdown-menu">
+					<li><a href="{$smarty.const.DIR_WS_HTTPS_ADMIN}countries.php">{tr}Countries{/tr}</a></li>
+					<li><a href="{$smarty.const.DIR_WS_HTTPS_ADMIN}currencies.php">{tr}Currencies{/tr}</a></li>
+					<li><a href="{$smarty.const.DIR_WS_HTTPS_ADMIN}languages.php">{tr}Languages{/tr}</a></li>
+					<li><a href="{$smarty.const.DIR_WS_HTTPS_ADMIN}zones.php">{tr}Zones{/tr}</a></li>
+					<li><a href="{$smarty.const.DIR_WS_HTTPS_ADMIN}geo_zones.php">{tr}Zones Definitions{/tr}</a></li>
+					<li><a href="{$smarty.const.DIR_WS_HTTPS_ADMIN}tax_classes.php">{tr}Tax Classes{/tr}</a></li>
+					<li><a href="{$smarty.const.DIR_WS_HTTPS_ADMIN}tax_rates.php">{tr}Tax Rates{/tr}</a></li>
+				</ul>
+			</li>
+
+            <li class="dropdown">
+                <a data-toggle="dropdown" class="dropdown-toggle" href="#">{tr}Reports{/tr} <b class="caret"></b></a>
+                <ul class="dropdown-menu">
+					<li><a href="{$smarty.const.DIR_WS_HTTPS_ADMIN}sales_and_income.php">{tr}Sales and Income{/tr}</a></li>
+					<li><a href="{$smarty.const.DIR_WS_HTTPS_ADMIN}stats_products_viewed.php">{tr}Products Viewed{/tr}</a></li>
+					<li><a href="{$smarty.const.DIR_WS_HTTPS_ADMIN}stats_products_types.php">{tr}Products Sales{/tr}</a></li>
+					<li><a href="{$smarty.const.DIR_WS_HTTPS_ADMIN}stats_products_purchased.php">{tr}Products Purchased{/tr}</a></li>
+					<li><a href="{$smarty.const.DIR_WS_HTTPS_ADMIN}stats_customers.php">{tr}Customer Orders{/tr}</a></li>
+					<li><a href="{$smarty.const.DIR_WS_HTTPS_ADMIN}stats_products_lowstock.php">{tr}Products Low Stock{/tr}</a></li>
+					<li><a href="{$smarty.const.DIR_WS_HTTPS_ADMIN}stats_customers_referrals.php">{tr}Customers Referral{/tr}</a></li>
+				</ul>
+			</li>
