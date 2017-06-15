5bfbfd6570f42904bbebb46dfb768562f8154af4
spider@dev <spiderr@bitweaver.org>
2017-06-15 17:05:07 -0400

    fix also_purchased_products module from crashing, crude bootstrap clean up

--- includes/modules/also_purchased_products.php
+++ includes/modules/also_purchased_products.php
@@ -19,48 +19,32 @@
 // +----------------------------------------------------------------------+
 // $Id$
 //
-  if (isset($_GET['products_id']) && SHOW_PRODUCT_INFO_COLUMNS_ALSO_PURCHASED_PRODUCTS > 0) {
-
-	$query = "SELECT p.`products_id`, p.`products_image`, pt.`type_class`
-              FROM " . TABLE_ORDERS_PRODUCTS . " opa
+global $gCommerceSystem;
+if (isset($_GET['products_id']) && $gCommerceSystem->getConfig( 'SHOW_PRODUCT_INFO_COLUMNS_ALSO_PURCHASED_PRODUCTS' ) ) {
+	global $gCommerceSystem;
+	$query = "SELECT p.`products_id`
+			  FROM " . TABLE_ORDERS_PRODUCTS . " opa
 				INNER JOIN " . TABLE_ORDERS_PRODUCTS . " opb ON (opa.`orders_id` = opb.`orders_id`)
 				INNER JOIN " . TABLE_ORDERS . " o ON (opb.`orders_id` = o.`orders_id`)
 				INNER JOIN " . TABLE_PRODUCTS . " p ON (opb.`products_id` = p.`products_id`)
 				INNER JOIN " . TABLE_PRODUCT_TYPES . " pt ON (p.`products_type` = pt.`type_id`)
-			  WHERE opa.`products_id` = ?  AND opb.`products_id` != ? AND p.`products_status` = '1'
-			  ORDER by o.`date_purchased` DESC";
+			  WHERE opa.`products_id` = ?	AND opb.`products_id` != ? AND p.`products_status` = '1'
+			  GROUP BY p.`products_id`, p.`products_image`, pt.`type_class`
+			  ORDER BY SUM( opb.`products_quantity` ) DESC";
 
-    $orders = $gBitDb->query( $query, array( (int)$_GET['products_id'], (int)$_GET['products_id']), MAX_DISPLAY_ALSO_PURCHASED );
+	if( $relatedProductIds = $gCommerceSystem->mDb->getCol( $query, array( (int)$_GET['products_id'], (int)$_GET['products_id']), MAX_DISPLAY_ALSO_PURCHASED ) ) {
 
-    $num_products_ordered = $orders->RecordCount();
-
-    if ($num_products_ordered > 0) {
-//    if ($num_products_ordered >= MIN_DISPLAY_ALSO_PURCHASED) {
-      $row = 0;
-      $col = 0;
-      $list_box_contents = '';
-      if ($num_products_ordered < SHOW_PRODUCT_INFO_COLUMNS_ALSO_PURCHASED_PRODUCTS) {
-        $col_width = 100/$num_products_ordered;
-      } else {
-        $col_width = 100/SHOW_PRODUCT_INFO_COLUMNS_ALSO_PURCHASED_PRODUCTS;
-      }
-      $info_box_contents = array();
-      while (!$orders->EOF) {
-        $orders->fields['products_name'] = zen_get_products_name($orders->fields['products_id']);
-        $orders->fields['products_name'] = zen_get_products_name($orders->fields['products_id']);
-		$typeClass = $orders->fields['type_class'];
-        $list_box_contents[$row][$col] = array('align' => 'center',
-                                               'params' => 'class="smallText" width="' . $col_width . '%" valign="top"',
-                                               'text' => '<a href="' . zen_href_link(zen_get_info_page($orders->fields['products_id']), 'products_id=' . $orders->fields['products_id']) . '">' . zen_image( $typeClass::getImageUrlFromHash( $orders->fields, 'avatar' ), $orders->fields['products_name'] ) . '</a><br /><a href="' . zen_href_link(zen_get_info_page($orders->fields['products_id']), 'products_id=' . $orders->fields['products_id']) . '">' . $orders->fields['products_name'] . '</a>');
-
-        $col ++;
-        if ($col > (SHOW_PRODUCT_INFO_COLUMNS_ALSO_PURCHASED_PRODUCTS - 1)) {
-          $col = 0;
-          $row ++;
-        }
-        $orders->MoveNext();
-      }
-      require( DIR_FS_MODULES . 'tpl_modules_also_purchased_products.php');
-    }
-  }
+		if( count( $relatedProductIds ) >= $gCommerceSystem->getConfig( 'MIN_DISPLAY_ALSO_PURCHASED' ) ) {
+			$list_box_contents = '';
+			$info_box_contents = array();
+			foreach( $relatedProductIds as $relatedProductId ) {
+				if( $listProduct = bc_get_commerce_product( array( 'products_id' => $relatedProductId ) ) ) {
+					$productTitle = htmlspecialchars( $listProduct->getTitle() );
+					$relatedList[$relatedProductId] = '<a href="'.$listProduct->getDisplayUrl().'"><img src="'.$listProduct->getThumbnailUrl('small').'" class="img-responsive" alt="'.$productTitle.'"/></a><br />'.$listProduct->getDisplayLink();
+				}
+			}
+			require( DIR_FS_MODULES . 'tpl_modules_also_purchased_products.php');
+		}
+	}
+}
 ?>
