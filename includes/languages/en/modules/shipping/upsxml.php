e5971c839dd7116382f69361f0d2b4435b091afc
spiderr <spiderr@bitweaver.org>
2019-05-21 18:16:17 -0400

    port upsxml 1.7.3

---
+++ includes/languages/en/modules/shipping/upsxml.php
@@ -0,0 +1,75 @@
+<?php
+/*
+    $Id: upsxml.php,v 1.1.2 2004/11/27 01:03:03 torinwalker Exp $
+    Written by Torin Walker
+    torinwalker@rogers.com
+
+    Original copyright (c) 2003 Torin Walker
+    Copyright(c) 2003 by Torin Walker, All rights reserved.
+
+    Released under the GNU General Public License
+    This program is free software; you can redistribute it and/or modify it 
+    under the terms of the GNU General Public License as published by the Free 
+    Software Foundation; either version 2 of the License, or (at your option) 
+    any later version. This program is distributed in the hope that it will be 
+    useful, but WITHOUT ANY WARRANTY; without even the implied warranty of 
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General 
+    Public License for more details. You should have received a copy of the 
+    GNU General Public License along with this program; If not, you may obtain 
+    one by writing to and requesting one from:
+    The Free Software Foundation, Inc.,
+    59 Temple Place, Suite 330,
+    Boston, MA 02111-1307 USA
+
+    Modifyed for zen-cart 1.2.5d by Dennis Sayer - July 9, 2005
+    Indention corrections by Dennis Sayer - July 9, 2005
+    dennis.s.sayer@brandnamebatteries.com
+
+*/
+define('MODULE_SHIPPING_UPSXML_TEXT_TITLE', 'United Parcel Service');
+define('MODULE_SHIPPING_UPSXML_TEXT_DESCRIPTION', 'United Parcel Service');
+define('MODULE_SHIPPING_UPSXML_TEXT_UNKNOWN_ERROR', 'An unknown error occured with the ups shipping calculations.');
+define('MODULE_SHIPPING_UPSXML_TEXT_IF_YOU_PREFER', 'If you prefer to use ups as your shipping method, please contact');
+define('MODULE_SHIPPING_UPSXML_TEXT_COMM_ERROR', 'A communication error occured while attempting to contact the UPS gateway');
+define('MODULE_SHIPPING_UPSXML_TEXT_COMM_UNKNOWN_ERROR', 'An unknown error occured while attempting to contact the UPS gateway');
+define('MODULE_SHIPPING_UPSXML_TEXT_COMM_VERSION_ERROR', 'This module supports only xpci version 1.0001 of the UPS Rates Interface. Please contact the webmaster for additional assistance.');
+define('MODULE_SHIPPING_UPSXML_SERVICE_CODE_US_ORIGIN_01', 'UPS Next Day Air');
+define('MODULE_SHIPPING_UPSXML_SERVICE_CODE_US_ORIGIN_02', 'UPS 2nd Day Air');
+define('MODULE_SHIPPING_UPSXML_SERVICE_CODE_US_ORIGIN_03', 'UPS Ground');
+define('MODULE_SHIPPING_UPSXML_SERVICE_CODE_US_ORIGIN_07', 'UPS Worldwide Express');
+define('MODULE_SHIPPING_UPSXML_SERVICE_CODE_US_ORIGIN_08', 'UPS Worldwide Expedited');
+define('MODULE_SHIPPING_UPSXML_SERVICE_CODE_US_ORIGIN_11', 'UPS Standard');
+define('MODULE_SHIPPING_UPSXML_SERVICE_CODE_US_ORIGIN_12', 'UPS 3 Day Select');
+define('MODULE_SHIPPING_UPSXML_SERVICE_CODE_US_ORIGIN_13', 'UPS Next Day Air Saver');
+define('MODULE_SHIPPING_UPSXML_SERVICE_CODE_US_ORIGIN_14', 'UPS Next Day Air Early A.M.');
+define('MODULE_SHIPPING_UPSXML_SERVICE_CODE_US_ORIGIN_54', 'UPS Worldwide Express Plus');
+define('MODULE_SHIPPING_UPSXML_SERVICE_CODE_US_ORIGIN_59', 'UPS 2nd Day Air A.M.');
+define('MODULE_SHIPPING_UPSXML_SERVICE_CODE_US_ORIGIN_65', 'UPS Express Saver');
+define('MODULE_SHIPPING_UPSXML_SERVICE_CODE_CANADA_ORIGIN_01', 'UPS Express');
+define('MODULE_SHIPPING_UPSXML_SERVICE_CODE_CANADA_ORIGIN_02', 'UPS Expedited');
+define('MODULE_SHIPPING_UPSXML_SERVICE_CODE_CANADA_ORIGIN_07', 'UPS Worldwide Express');
+define('MODULE_SHIPPING_UPSXML_SERVICE_CODE_CANADA_ORIGIN_08', 'UPS Worldwide Expedited');
+define('MODULE_SHIPPING_UPSXML_SERVICE_CODE_CANADA_ORIGIN_11', 'UPS Standard');
+define('MODULE_SHIPPING_UPSXML_SERVICE_CODE_CANADA_ORIGIN_12', 'UPS 3 Day Select');
+define('MODULE_SHIPPING_UPSXML_SERVICE_CODE_CANADA_ORIGIN_13', 'UPS Express Saver');
+define('MODULE_SHIPPING_UPSXML_SERVICE_CODE_CANADA_ORIGIN_14', 'UPS Express Early A.M.');
+define('MODULE_SHIPPING_UPSXML_SERVICE_CODE_CANADA_ORIGIN_54', 'UPS Worldwide Express Plus');
+define('MODULE_SHIPPING_UPSXML_SERVICE_CODE_EU_ORIGIN_07', 'UPS Express');
+define('MODULE_SHIPPING_UPSXML_SERVICE_CODE_EU_ORIGIN_08', 'UPS Expedited');
+define('MODULE_SHIPPING_UPSXML_SERVICE_CODE_EU_ORIGIN_11', 'UPS Standard');
+define('MODULE_SHIPPING_UPSXML_SERVICE_CODE_EU_ORIGIN_54', 'UPS Worldwide Express Plus');
+define('MODULE_SHIPPING_UPSXML_SERVICE_CODE_EU_ORIGIN_64', 'UPS Express NA1');
+define('MODULE_SHIPPING_UPSXML_SERVICE_CODE_EU_ORIGIN_65', 'UPS Express Saver');
+define('MODULE_SHIPPING_UPSXML_SERVICE_CODE_PR_ORIGIN_01', 'UPS Next Day Air');
+define('MODULE_SHIPPING_UPSXML_SERVICE_CODE_PR_ORIGIN_02', 'UPS 2nd Day Air');
+define('MODULE_SHIPPING_UPSXML_SERVICE_CODE_PR_ORIGIN_03', 'UPS Ground');
+define('MODULE_SHIPPING_UPSXML_SERVICE_CODE_PR_ORIGIN_07', 'UPS Worldwide Express');
+define('MODULE_SHIPPING_UPSXML_SERVICE_CODE_PR_ORIGIN_08', 'UPS Worldwide Expedited');
+define('MODULE_SHIPPING_UPSXML_SERVICE_CODE_PR_ORIGIN_14', 'UPS Next Day Air&reg; Early A.M.');
+define('MODULE_SHIPPING_UPSXML_SERVICE_CODE_PR_ORIGIN_54', 'UPS Worldwide Express Plus');
+define('MODULE_SHIPPING_UPSXML_SERVICE_CODE_MEXICO_ORIGIN_07', 'UPS Worldwide Express');
+define('MODULE_SHIPPING_UPSXML_SERVICE_CODE_MEXICO_ORIGIN_08', 'UPS Worldwide Expedited');
+define('MODULE_SHIPPING_UPSXML_SERVICE_CODE_MEXICO_ORIGIN_54', 'UPS Worldwide Express Plus');
+define('MODULE_SHIPPING_UPSXML_SERVICE_CODE_OTHER_ORIGIN_07', 'UPS Worldwide Express');
+define('MODULE_SHIPPING_UPSXML_SERVICE_CODE_OTHER_ORIGIN_08', 'UPS Worldwide Expedited');
+define('MODULE_SHIPPING_UPSXML_SERVICE_CODE_OTHER_ORIGIN_54', 'UPS Worldwide Express Plus');
