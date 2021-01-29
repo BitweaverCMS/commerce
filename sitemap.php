340e5e334c68d0b0e91931cca11ccb0133e319fb
spiderr <spiderr@bitweaver.org>
2021-01-29 13:47:10 -0500

    check for display_url in sitemap

--- sitemap.php
+++ sitemap.php
@@ -8,20 +8,22 @@ $listHash['commissioned'] = TRUE;
 
 if( $productList = $gBitProduct->getList($listHash) ) {
 	foreach( $productList as $key=>$hash ) {
-		$newHash = array();
-		$newHash['loc'] =  BIT_BASE_URI.$hash['display_url'];
-		$newHash['priority'] = .8;
-		$lastMod = strtotime( $hash['products_last_modified'] );
-		$newHash['lastmod'] = date( 'Y-m-d', $lastMod );
-		if( (time() - $lastMod) < 86400 ) {
-			$freq = 'daily';
-		} elseif( (time() - $lastMod) < (86400 * 7) ) {
-			$freq = 'weekly';
-		} else {
-			$freq = 'monthly';
+		if( !empty( $hash['display_url'] ) ) {
+			$newHash = array();
+			$newHash['loc'] =  BIT_BASE_URI.$hash['display_url'];
+			$newHash['priority'] = .8;
+			$lastMod = strtotime( $hash['products_last_modified'] );
+			$newHash['lastmod'] = date( 'Y-m-d', $lastMod );
+			if( (time() - $lastMod) < 86400 ) {
+				$freq = 'daily';
+			} elseif( (time() - $lastMod) < (86400 * 7) ) {
+				$freq = 'weekly';
+			} else {
+				$freq = 'monthly';
+			}
+			$newHash['changefreq'] = $freq;
+			$gSiteMapHash[$key] = $newHash;
 		}
-		$newHash['changefreq'] = $freq;
-		$gSiteMapHash[$key] = $newHash;
 	}
 }
 
