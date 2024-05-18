324296be6edec0c630b2b500a8929002acad8b5d
spiderr <spiderr@bitweaver.org>
2024-05-18 14:17:47 -0400

    fix GV constants

--- includes/languages/en/modules/order_total/ot_coupon.php
+++ includes/languages/en/modules/order_total/ot_coupon.php
@@ -21,9 +21,9 @@
 //
 
   define('MODULE_ORDER_TOTAL_COUPON_TITLE', tra( 'Discount Coupons' ) );
-  define('MODULE_ORDER_TOTAL_COUPON_HEADER', TEXT_GV_NAMES . '/Discount Coupons');
+  define('MODULE_ORDER_TOTAL_COUPON_HEADER', tra( 'Gift Certificates' ) . '/' . tra( 'Discount Coupons' ) );
   define('MODULE_ORDER_TOTAL_COUPON_DESCRIPTION', tra( 'Discount Coupons' ) );
-  define('MODULE_ORDER_TOTAL_COUPON_TEXT_ENTER_CODE', TEXT_GV_REDEEM);
+  define('MODULE_ORDER_TOTAL_COUPON_TEXT_ENTER_CODE', tra('Redemption Code') );
   define('SHIPPING_NOT_INCLUDED', tra( ' [Shipping not included]' ) );
   define('TAX_NOT_INCLUDED', tra( ' [Tax not included]' ) );
   define('IMAGE_REDEEM_VOUCHER', tra( 'Redeem Voucher' ) );
