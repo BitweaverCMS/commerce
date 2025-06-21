398968288b2d0857588eccb39738c1e62771b242
spiderr <spiderr@bitweaver.org>
2025-06-21 09:27:33 -0400

    remove debug

--- includes/languages/en.php
+++ includes/languages/en.php
@@ -57,7 +57,6 @@ define('FOOTER_TEXT_REQUESTS_SINCE', 'requests since');
 // Define the name of your Gift Certificate as Gift Voucher, Gift Certificate, Zen Cart Dollars, etc. here for use through out the shop
   define('TEXT_GV_NAME','Gift Certificate');
   define('TEXT_GV_NAMES','Gift Certificates');
-bt();
 
 // used for redeem code, redemption code, or redemption id
   define('TEXT_GV_REDEEM','Redemption Code');
