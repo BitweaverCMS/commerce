4de1ba996076baa72b6da3c841193176ffb4a2b1
spiderr <spiderr@bitweaver.org>
2025-12-29 09:06:56 -0500

    remove USPS hack

--- admin/modules.php
+++ admin/modules.php
@@ -28,9 +28,6 @@ if( $moduleType == 'shipping' ) {
 	if (zen_get_configuration_key_value('ORDER_WEIGHT_ZERO_STATUS') == '1' and !defined('MODULE_SHIPPING_FREESHIPPER_STATUS')) {
 		$shipping_errors .= '<br />' . ERROR_ORDER_WEIGHT_ZERO_STATUS;
 	}
-	if( defined('MODULE_SHIPPING_USPS_STATUS') and (MODULE_SHIPPING_USPS_USERID=='NONE' or MODULE_SHIPPING_USPS_SERVER == 'test')) {
-		$shipping_errors .= '<br />' . ERROR_USPS_STATUS;
-	}
 	if ($shipping_errors != '') {
 		$messageStack->add(ERROR_SHIPPING_CONFIGURATION . $shipping_errors, 'caution');
 	}
